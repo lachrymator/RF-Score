c6928b1b62803d51e20dfbe85f1ee46f7f27af3c
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-09-22 16:59:30 +0800

    Revised rf-prepare.cpp to accept rf_core_data.2012 instead of INDEX_core_data.2012

--- rf-prepare.cpp
+++ rf-prepare.cpp
@@ -7,11 +7,11 @@ int main(int argc, char* argv[])
 {
 	if (argc != 3)
 	{
-		cout << "Usage: rf-prepare /path/to/INDEX_core_data.2012 rf-test.csv" << endl;
+		cout << "Usage: rf-prepare /path/to/rf_core_data.2012 rf-test.csv" << endl;
 		return 0;
 	}
 
-	// Load a receptor and a ligand to calculate RF-Score features and Vina terms.
+	// Traverse the PDBbind folder to extract RF-Score features and Vina terms.
 	const string path = argv[1];
 	const string root = path.substr(0, path.find_last_of("/\\") + 1);
 	string line;
@@ -20,9 +20,8 @@ int main(int argc, char* argv[])
 	ofs << "pbindaff,6.6,7.6,8.6,16.6,6.7,7.7,8.7,16.7,6.8,7.8,8.8,16.8,6.16,7.16,8.16,16.16,6.15,7.15,8.15,16.15,6.9,7.9,8.9,16.9,6.17,7.17,8.17,16.17,6.35,7.35,8.35,16.35,6.53,7.53,8.53,16.53,gauss1,gauss2,repulsion,hydrophobic,hydrogenbonding,PDB\n" << setprecision(2);
 	for (ifstream dataifs(argv[1]); getline(dataifs, line);)
 	{
-		if (line[0] == '#') continue;
 		const string code = line.substr(0, 4);
-		const float pbindaff = stof(line.substr(18, 6));
+		const float pbindaff = stof(line.substr(5));
 		receptor rec;
 		rec.load(root + code + "/" + code + "_protein.pdbqt");
 		ligand lig;
