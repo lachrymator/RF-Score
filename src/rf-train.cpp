d4a531dc40b58c70238f2bafe35e22488b778613
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-02 15:26:15 +0800

    Tried more mtry values

--- src/rf-train.cpp
+++ src/rf-train.cpp
@@ -40,11 +40,12 @@ int main(int argc, char* argv[])
 	ifs.close();
 
 	// Train random forests of different mtry values in parallel.
+	const size_t num_variables = x.front().size();
 	const size_t num_trees = 500;
 	const size_t seed = argc == 3 ? 89757 : stoul(argv[3]); // time(0), random_device()()
 	const size_t num_threads = 4;
 	const size_t min_mtry = 1;
-	const size_t max_mtry = 1 + x.front().size() / 4;
+	const size_t max_mtry = num_variables <= 6 ? num_variables : 1 + num_variables / 3;
 	vector<forest> forests(max_mtry - min_mtry + 1);
 	cout << "Training " << forests.size() << " random forests of " << num_trees << " trees with mtry from " << min_mtry << " to " << max_mtry << " and seed " << seed << " using " << num_threads << " threads" << endl;
 	vector<thread> threads;
