310dc359ec769ca153ce862892bb4edc642aaf85
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-02 15:26:00 +0800

    Changed setprecision from 2 to 4

--- src/rf-prepare.cpp
+++ src/rf-prepare.cpp
@@ -18,7 +18,7 @@ int main(int argc, char* argv[])
 	string line;
 	ofstream ofs(argv[2]);
 	ofs.setf(ios::fixed, ios::floatfield);
-	ofs << "pbindaff,6.6,7.6,8.6,16.6,6.7,7.7,8.7,16.7,6.8,7.8,8.8,16.8,6.16,7.16,8.16,16.16,6.15,7.15,8.15,16.15,6.9,7.9,8.9,16.9,6.17,7.17,8.17,16.17,6.35,7.35,8.35,16.35,6.53,7.53,8.53,16.53,gauss1,gauss2,repulsion,hydrophobic,hydrogenbonding,PDB\n" << setprecision(2);
+	ofs << "pbindaff,6.6,7.6,8.6,16.6,6.7,7.7,8.7,16.7,6.8,7.8,8.8,16.8,6.16,7.16,8.16,16.16,6.15,7.15,8.15,16.15,6.9,7.9,8.9,16.9,6.17,7.17,8.17,16.17,6.35,7.35,8.35,16.35,6.53,7.53,8.53,16.53,gauss1,gauss2,repulsion,hydrophobic,hydrogenbonding,PDB\n" << setprecision(4);
 	for (ifstream dataifs(argv[1]); getline(dataifs, line);)
 	{
 		const string code = line.substr(0, 4);
