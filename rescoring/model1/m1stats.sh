61b03307a4021e3782ca4310ad96c93eb03ef363
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-06 17:15:07 +0800

    Replaced m1stats.sh by stats.sh. Updated model 1 statistics.csv

--- rescoring/model1/m1stats.sh
+++
@@ -1,4 +0,0 @@
-echo rmse,sdev,pcor,scor,kcor > pdbbind2007-core-statistics.csv
-cp pdbbind2007-core-statistics.csv pdbbind2007-refined-core-statistics.csv
-tail -n +2 rf-stat-core.txt | cut -d' ' -f2 | tr '\n' ',' | sed 's/,$/\n/g' >> pdbbind2007-core-statistics.csv
-tail -n +2 rf-stat-refined-core.txt | cut -d' ' -f2 | tr '\n' ',' | sed 's/,$/\n/g' >> pdbbind2007-refined-core-statistics.csv
