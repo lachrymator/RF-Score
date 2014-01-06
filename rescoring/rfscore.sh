bbc4e70a449589aaf118a6abb64b5c4554f8e1e9
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-06 18:53:29 +0800

    Updated the output filename of rf-test

--- rescoring/rfscore.sh
+++ rescoring/rfscore.sh
@@ -3,14 +3,14 @@
 rf-prepare ~/PDBbind/v2007/pdbbind2007-core-iy.csv pdbbind2007-core-yxi.csv
 rf-prepare ~/PDBbind/v2007/pdbbind2007-refined-core-iy.csv pdbbind2007-refined-core-yxi.csv
 echo seed,n,rmse,sdev,pcor,scor,kcor > pdbbind2007-core-statistics.csv
-cp pdbbind2007-core-statistics.csv pdbbind2007-refined-core-statistics.csv
+echo seed,n,rmse,sdev,pcor,scor,kcor > pdbbind2007-refined-core-statistics.csv
 for s in $(cat ../seed.csv); do
 	echo $s
 	mkdir -p $s
 	cd $s
 	rf-train ../pdbbind2007-refined-core-yxi.csv pdbbind2007-refined-core.rf $s > rf-train-refined-core.txt
-	rf-test pdbbind2007-refined-core.rf ../pdbbind2007-refined-core-yxi.csv pdbbind2007-refined-core-iyp.csv > rf-test-refined-core.txt
-	rf-test pdbbind2007-refined-core.rf ../pdbbind2007-core-yxi.csv pdbbind2007-core-iyp.csv > rf-test-core.txt
+	rf-test pdbbind2007-refined-core.rf ../pdbbind2007-refined-core-yxi.csv pdbbind2007-refined-core-iyp.csv > pdbbind2007-refined-core-statistics.csv
+	rf-test pdbbind2007-refined-core.rf ../pdbbind2007-core-yxi.csv pdbbind2007-core-iyp.csv > pdbbind2007-core-statistics.csv
 	cd ..
 	echo -n $s, >> pdbbind2007-core-statistics.csv
 	echo -n $s, >> pdbbind2007-refined-core-statistics.csv
