f644ea64092496d5947bbf0ba74b15c755a853e6
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-06 22:31:16 +0800

    Output to file instead of cout

--- rescoring/model2/m2score.R
+++ rescoring/model2/m2score.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 w=as.numeric(commandArgs(trailingOnly=T)[1])
-d=read.csv('pdbbind2007-refined-core-yxi.csv') # d[1] is pbindaff in pKd unit, d[2] to d[6] are Vina's 5 unweighted terms, d[7] and d[8] are N(ActTors) and N(InactTors).
+d=read.csv('../pdbbind2007-refined-core-yxi.csv') # d[1] is pbindaff in pKd unit, d[2] to d[6] are Vina's 5 unweighted terms, d[7] and d[8] are N(ActTors) and N(InactTors).
 d['t']=1+w*(d[7]+0.5*d[8]) # Flexibility penalty = 1 + w * Nrot. In Vina's implementation, active torsions are counted as 1, while inactive torsions (i.e. -OH,-NH2,-CH3) are counted as 0.5.
 d['z']=d['pbindaff']*d['t'] # Measured pKd times the flexibility penalty.
 r=lm(z~gauss1+gauss2+repulsion+hydrophobic+hydrogenbonding,d) # Linear regression of Vina's 5 unweighted terms.
@@ -17,10 +17,10 @@ sdev=sqrt(se/(n-1))
 pcor=cor(d['p'], d[1], method="pearson")
 scor=cor(d['p'], d[1], method="spearman")
 kcor=cor(d['p'], d[1], method="kendall")
-cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor))
+cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor), file="pdbbind2007-refined-core-statistics.csv")
 
 # Evaluate on test set.
-d=read.csv('pdbbind2007-core-yxi.csv') # d[1] is pbindaff in pKd unit, d[2] to d[6] are Vina's 5 unweighted terms, d[7] and d[8] are N(ActTors) and N(InactTors).
+d=read.csv('../pdbbind2007-core-yxi.csv') # d[1] is pbindaff in pKd unit, d[2] to d[6] are Vina's 5 unweighted terms, d[7] and d[8] are N(ActTors) and N(InactTors).
 d['t']=1+w*(d[7]+0.5*d[8]) # Flexibility penalty = 1 + w * Nrot. In Vina's implementation, active torsions are counted as 1, while inactive torsions (i.e. -OH,-NH2,-CH3) are counted as 0.5.
 d['p']=(c[1]+d[2]*c[2]+d[3]*c[3]+d[4]*c[4]+d[5]*c[5]+d[6]*c[6])/d['t'] # Binding affinity predicted by the newly-trained model, with kcal/mol converted to pKd.
 #write.csv(c(d['PDB'],round(d['pbindaff'],2),round(d['p'],2)),row.names=F,quote=F,file="pdbbind2007-refined-core-iyp.csv") # Write the measured binding affinities and the predicted ones in CSV format.
@@ -32,4 +32,4 @@ sdev=sqrt(se/(n-1))
 pcor=cor(d['p'], d[1], method="pearson")
 scor=cor(d['p'], d[1], method="spearman")
 kcor=cor(d['p'], d[1], method="kendall")
-cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor))
+cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor), file="pdbbind2007-core-statistics.csv")
