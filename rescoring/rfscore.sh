4ba70269f77d56edb0a38a5c93640f9926b1b51f
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-06 17:28:50 +0800

    Updated rfscore.sh to parse the CSV output by rf-stat

--- rescoring/rfscore.sh
+++ rescoring/rfscore.sh
@@ -2,7 +2,7 @@
 #make
 rf-prepare ~/PDBbind/v2007/pdbbind2007-core-iy.csv pdbbind2007-core-yxi.csv
 rf-prepare ~/PDBbind/v2007/pdbbind2007-refined-core-iy.csv pdbbind2007-refined-core-yxi.csv
-echo seed,rmse,sdev,pcor,scor,kcor > pdbbind2007-core-statistics.csv
+echo seed,n,rmse,sdev,pcor,scor,kcor > pdbbind2007-core-statistics.csv
 cp pdbbind2007-core-statistics.csv pdbbind2007-refined-core-statistics.csv
 for s in $(cat ../seed.csv); do
 	echo $s
@@ -14,6 +14,6 @@ for s in $(cat ../seed.csv); do
 	cd ..
 	echo -n $s, >> pdbbind2007-core-statistics.csv
 	echo -n $s, >> pdbbind2007-refined-core-statistics.csv
-	tail -n +2 $s/rf-test-core.txt | cut -d' ' -f2 | tr '\n' ',' | sed 's/,$/\n/g' >> pdbbind2007-core-statistics.csv
-	tail -n +2 $s/rf-test-refined-core.txt | cut -d' ' -f2 | tr '\n' ',' | sed 's/,$/\n/g' >> pdbbind2007-refined-core-statistics.csv
+	tail -1 $s/pdbbind2007-core-statistics.csv >> pdbbind2007-core-statistics.csv
+	tail -1 $s/pdbbind2007-refined-core-statistics.csv >> pdbbind2007-refined-core-statistics.csv
 done
