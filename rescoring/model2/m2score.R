ed7778a802d86c17e9748254eb9a94cc28490ce2
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-13 23:19:04 +0800

    Wrote pdbbind-%s-core-iyp.csv and pdbbind-%s-refined-core-iyp.csv. Plotted pdbbind-%s-core-iyp.tiff

--- rescoring/model2/m2score.R
+++ rescoring/model2/m2score.R
@@ -9,7 +9,7 @@ c=coefficients(r) # Trained weights.
 
 # Evaluate on training set.
 d["p"]=(c[1]+d[2]*c[2]+d[3]*c[3]+d[4]*c[4]+d[5]*c[5]+d[6]*c[6])/d["t"] # Binding affinity predicted by the newly-trained model, with kcal/mol converted to pKd.
-#write.csv(c(d["PDB"],round(d["pbindaff"],2),round(d["p"],2)),row.names=F,quote=F,file=sprintf("pdbbind-%s-refined-core-iyp.csv",v)) # Write the measured binding affinities and the predicted ones in CSV format.
+write.csv(c(d["PDB"],round(d["pbindaff"],2),round(d["p"],2)),row.names=F,quote=F,file=sprintf("pdbbind-%s-refined-core-iyp.csv",v)) # Write the measured binding affinities and the predicted ones in CSV format.
 # Print RMSE, standard deviation, Pearson/Spearman/Kendall correlation coefficients.
 n=nrow(d) # Number of samples.
 se=sum((d["p"] - d[1])^2) # Square error.
@@ -24,7 +24,7 @@ cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse
 d=read.csv("../pdbbind-2007-core-yxi.csv") # d[1] is pbindaff in pKd unit, d[2] to d[6] are Vina"s 5 unweighted terms, d[7] and d[8] are N(ActTors) and N(InactTors).
 d["t"]=1+w*(d[7]+0.5*d[8]) # Flexibility penalty = 1 + w * Nrot. In Vina"s implementation, active torsions are counted as 1, while inactive torsions (i.e. -OH,-NH2,-CH3) are counted as 0.5.
 d["p"]=(c[1]+d[2]*c[2]+d[3]*c[3]+d[4]*c[4]+d[5]*c[5]+d[6]*c[6])/d["t"] # Binding affinity predicted by the newly-trained model, with kcal/mol converted to pKd.
-#write.csv(c(d["PDB"],round(d["pbindaff"],2),round(d["p"],2)),row.names=F,quote=F,file=sprintf("pdbbind-2007-refined-core-iyp.csv",v)) # Write the measured binding affinities and the predicted ones in CSV format.
+write.csv(c(d["PDB"],round(d["pbindaff"],2),round(d["p"],2)),row.names=F,quote=F,file=sprintf("pdbbind-%s-core-iyp.csv",v)) # Write the measured binding affinities and the predicted ones in CSV format.
 # Print RMSE, standard deviation, Pearson/Spearman/Kendall correlation coefficients.
 n=nrow(d) # Number of samples.
 se=sum((d["p"] - d[1])^2) # Square error.
--- rescoring/model2/m2score.sh
+++ rescoring/model2/m2score.sh
@@ -7,11 +7,12 @@ for v in 2004 2007 2010 2013; do
 		mkdir -p $s
 		cd $s
 		../m2score.R $v $s
+		../../plot.R $v
 		cd ..
 		echo -n $s, >> pdbbind-$v-core-statistics.csv
 		echo -n $s, >> pdbbind-$v-refined-core-statistics.csv
 		tail -1 $s/pdbbind-$v-core-statistics.csv >> pdbbind-$v-core-statistics.csv
 		tail -1 $s/pdbbind-$v-refined-core-statistics.csv >> pdbbind-$v-refined-core-statistics.csv
-		rm -rf $s
+#		rm -rf $s
 	done
 done
