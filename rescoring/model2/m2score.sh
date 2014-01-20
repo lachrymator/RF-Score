0fc629cf28467dd3cced179d2b687280db9c5417
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-20 17:17:06 +0800

    Added set loop

--- rescoring/model2/m2score.sh
+++ rescoring/model2/m2score.sh
@@ -1,18 +1,23 @@
-for v in $(ls -1 pdbbind-*-trn-yxi.csv | ~/idock/utilities/substr 8 4); do
-	echo $v
-	echo weight,n,rmse,sdev,pcor,scor,kcor > pdbbind-$v-tst-stat.csv
-	echo weight,n,rmse,sdev,pcor,scor,kcor > pdbbind-$v-trn-stat.csv
-	for s in $(cat ../weight.csv); do
-		echo $s
-		mkdir -p $s
-		cd $s
-		../../m2score.R $v $s
-		../../../plot.R $v
-		cd ..
-		echo -n $s, >> pdbbind-$v-tst-stat.csv
-		echo -n $s, >> pdbbind-$v-trn-stat.csv
-		tail -1 $s/pdbbind-$v-tst-stat.csv >> pdbbind-$v-tst-stat.csv
-		tail -1 $s/pdbbind-$v-trn-stat.csv >> pdbbind-$v-trn-stat.csv
-#		rm -rf $s
+for s in 1 2; do
+	echo set$s
+	cd set$s
+	for v in $(ls -1 pdbbind-*-trn-yxi.csv | ~/idock/utilities/substr 8 4); do
+		echo $v
+		echo weight,n,rmse,sdev,pcor,scor,kcor > pdbbind-$v-tst-stat.csv
+		echo weight,n,rmse,sdev,pcor,scor,kcor > pdbbind-$v-trn-stat.csv
+		for s in $(seq 0.005 0.001 0.020); do
+			echo $s
+			mkdir -p $s
+			cd $s
+			../../m2score.R $v $s
+			../../../plot.R $v
+			cd ..
+			echo -n $s, >> pdbbind-$v-tst-stat.csv
+			echo -n $s, >> pdbbind-$v-trn-stat.csv
+			tail -1 $s/pdbbind-$v-tst-stat.csv >> pdbbind-$v-tst-stat.csv
+			tail -1 $s/pdbbind-$v-trn-stat.csv >> pdbbind-$v-trn-stat.csv
+#			rm -rf $s
+		done
 	done
+	cd ..
 done
