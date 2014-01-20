83b8a69ac82dea451d90851a5f206f283695d225
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-20 17:26:39 +0800

    Added boxplot.sh

---
+++ rescoring/boxplot.sh
@@ -0,0 +1,9 @@
+for s in 1 2; do
+	echo set$s
+	for v in $(ls -1 model2/set${s}/pdbbind-*-trn-yxi.csv | ~/idock/utilities/substr 20 4); do
+		echo $v
+		./boxplot.R $s $v
+	done
+	mkdir -p set$s
+	mv *.tiff set$s
+done
