95f6a00c29e5419bbe30ed62e33c1821931d1af6
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-20 23:12:55 +0800

    Fixed a typo

--- rescoring/xmdl.sh
+++ rescoring/xmdl.sh
@@ -2,7 +2,7 @@ for s in 1 2; do
 	echo set$s
 	for v in $(ls -1 model2/set${s}/pdbbind-*-trn-yxi.csv | ~/idock/utilities/substr 20 4); do
 		echo $v
-		./boxplot.R $s $v
+		./xmdl.R $s $v
 	done
 	mkdir -p set$s
 	mv *.tiff set$s
