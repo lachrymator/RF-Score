a2968ae2c8eee3a5c1726a9607b624120a4bf924
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-21 11:07:39 +0800

    Enlarged the height of tiff if nrow(imp) > 6

--- rescoring/varImpPlot.R
+++ rescoring/varImpPlot.R
@@ -2,8 +2,13 @@
 v=commandArgs(trailingOnly=T)[1]
 labels=c("gauss1","gauss2","repulsion","hydrophobic","hydrogenbonding","flexibility")
 imp=read.table(file("stdin"))
-if (nrow(imp) > 6) labels=c(6.6,7.6,8.6,16.6,6.7,7.7,8.7,16.7,6.8,7.8,8.8,16.8,6.16,7.16,8.16,16.16,6.15,7.15,8.15,16.15,6.9,7.9,8.9,16.9,6.17,7.17,8.17,16.17,6.35,7.35,8.35,16.35,6.53,7.53,8.53,16.53,labels)
+h=480
+if (nrow(imp) > 6)
+{
+	labels=c(6.6,7.6,8.6,16.6,6.7,7.7,8.7,16.7,6.8,7.8,8.8,16.8,6.16,7.16,8.16,16.16,6.15,7.15,8.15,16.15,6.9,7.9,8.9,16.9,6.17,7.17,8.17,16.17,6.35,7.35,8.35,16.35,6.53,7.53,8.53,16.53,labels)
+	h=800
+}
 ord=order(imp)
-tiff(sprintf("pdbbind-%s-trn-varimpplot.tiff",v), compression="lzw")
+tiff(sprintf("pdbbind-%s-trn-varimpplot.tiff",v), compression="lzw", height=h)
 par(cex=1.3)
 dotchart(imp[ord,1], labels=labels[ord], xlab="%IncMSE", main="Variable Importance")
