b3a6849575c6a22879cc0b33a3993323e86f4038
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-22 18:05:54 +0800

    Integrated xmdl into xtrn.R

--- rescoring/xmdl.sh
+++
@@ -1,9 +0,0 @@
-for s in 1 2; do
-	echo set$s
-	for v in $(ls -1 model2/set${s}/pdbbind-*-trn-yxi.csv | ~/idock/utilities/substr 20 4); do
-		echo $v
-		./xmdl.R $s $v
-	done
-	mkdir -p xmdl/set$s
-	mv *.tiff xmdl/set$s
-done
--- rescoring/xtrn.R
+++ rescoring/xtrn.R
@@ -1,18 +1,19 @@
 #!/usr/bin/env Rscript
-nc=5 # Number of statistic code.
-statc=c("rmse","sdev","pcor","scor","kcor")
-statx=c("RMSE","SD","Rp","Rs","Rk")
+nm=4 # Number of models.
 ns=2 # Number of datasets.
-nv=4 # Number of PDBbind versions, i.e. training sets per dataset.
+nv=4 # Number of training sets per dataset.
+nc=5 # Number of performance measures.
 setv=array(dim=c(ns,nv))
 setv[1,]=c(2004,2007,2010,2013)
 setv[2,]=c(2002,2007,2010,2012)
+statc=c("rmse","sdev","pcor","scor","kcor")
+statx=c("RMSE","SD","Rp","Rs","Rk")
 # Plot figures with y axis being the performance measure and x axis being the numbers of training complexes.
-print("xtrn")
+cat(sprintf("xtrn\n"))
 for (m in 2:5)
 {
 	cat(sprintf("model%d\n",m))
-	for (s in 1:2)
+	for (s in 1:ns)
 	{
 		cat(sprintf("set%d\n",s))
 		ntrn=array(dim=nv)
@@ -45,13 +46,36 @@ for (m in 2:5)
 	}
 }
 # Plot figures with y axis being the performance measure and x axis being the models trained on a specific training set.
-print("xmdl")
-for (s in 1:2)
+cat(sprintf("xmdl\n"))
+for (s in 1:ns)
 {
 	cat(sprintf("set%d\n",s))
 	for (vi in 1:nv)
 	{
 		v=setv[s,vi]
 		cat(sprintf("v%d\n",v))
+		box=array(list(),dim=c(nc,nm))
+		med=array(dim=c(nc,nm))
+		for (m in 1:nm)
+		{
+			tst_stat=read.csv(sprintf("model%d/set%s/pdbbind-%s-tst-stat.csv",m,s,ifelse(m==1,2007,v)))
+			for (si in 1:nc)
+			{
+				box[si,m]=tst_stat[statc[si]]
+				med[si,m]=median(tst_stat[statc[si]][,])
+			}
+		}
+		for (si in 1:nc)
+		{
+			tiff(sprintf("set%d/pdbbind-%s-tst-%s-boxplot.tiff",s,v,statc[si]),compression="lzw")
+			par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
+			boxplot(box[si,],main=sprintf("Boxplot of %s",statx[si]),xlab="Model",ylab=statx[si],range=0)
+			dev.off()
+			tiff(sprintf("set%d/pdbbind-%s-tst-%s-median.tiff",s,v,statc[si]),compression="lzw")
+			par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
+			plot(1:4,med[si,],main=sprintf("Median of %s",statx[si]),xlab="Model",ylab=statx[si],pch=3,xaxt="n")
+			axis(1,1:4)
+			dev.off()
+		}
 	}
 }
