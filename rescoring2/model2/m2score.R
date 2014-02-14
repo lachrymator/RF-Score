b4026e405cbdaeac0cbe0f74ff320882c886b3fc
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-02-14 23:01:45 +0800

    Changed tst-yxi.csv to tst-%s-yxi.csv

--- rescoring2/model2/m2score.R
+++ rescoring2/model2/m2score.R
@@ -24,7 +24,7 @@ kcor=cor(d["p"], d[1], method="kendall")
 cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor), file=sprintf("pdbbind-%s-trn-%s-trn-%s-stat.csv",v,trn,trn))
 
 # Evaluate on test set.
-d=read.csv("../tst-yxi.csv") # d[1] is pbindaff in pKd unit, d[2] to d[6] are Vina"s 5 unweighted terms, d[7] and d[8] are N(ActTors) and N(InactTors).
+d=read.csv(sprintf("../tst-%s-yxi.csv",tst)) # d[1] is pbindaff in pKd unit, d[2] to d[6] are Vina"s 5 unweighted terms, d[7] and d[8] are N(ActTors) and N(InactTors).
 d["t"]=1+w*(d[7]+0.5*d[8]) # Flexibility penalty = 1 + w * Nrot. In Vina"s implementation, active torsions are counted as 1, while inactive torsions (i.e. -OH,-NH2,-CH3) are counted as 0.5.
 d["p"]=(c[1]+d[2]*c[2]+d[3]*c[3]+d[4]*c[4]+d[5]*c[5]+d[6]*c[6])/d["t"] # Binding affinity predicted by the newly-trained model, with kcal/mol converted to pKd.
 write.csv(c(d["PDB"],round(d["pbindaff"],2),round(d["p"],2)),row.names=F,quote=F,file=sprintf("pdbbind-%s-trn-%s-tst-%s-iyp.csv",v,trn,tst)) # Write the measured binding affinities and the predicted ones in CSV format.
