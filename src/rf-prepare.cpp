8413050219167aa097488ea8bea6e34cd64918cd
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-02-14 16:23:35 +0800

    Extended rf-prepare to support five schemes

--- src/rf-prepare.cpp
+++ src/rf-prepare.cpp
@@ -6,15 +6,16 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
+	if (argc < 3)
 	{
-		cout << "Usage: rf-prepare /path/to/PDBbind/v2012/pdbbind-2012-core-iy.csv pdbbind-2012-core-yx42i.csv" << endl;
+		cout << "Usage: rf-prepare /path/to/PDBbind/v2012/pdbbind-2012-core-iy.csv pdbbind-2012-core-yx42i.csv scheme" << endl;
 		return 0;
 	}
 
 	// Traverse the PDBbind folder to extract RF-Score features and Vina terms.
 	const string path = argv[1];
 	const string root = path.substr(0, path.find_last_of("/\\") + 1);
+	const string scheme = argc == 3 ? "1" : argv[3];
 	string line;
 	ofstream ofs(argv[2]);
 	ofs.setf(ios::fixed, ios::floatfield);
@@ -25,9 +26,51 @@ int main(int argc, char* argv[])
 		const float pbindaff = stof(line.substr(5));
 		receptor rec;
 		rec.load(root + code + "/" + code + "_protein.pdbqt");
-		ligand lig;
-		lig.load(root + code + "/" + code + "_ligand.pdbqt");
-		const vector<float> v = feature(rec, lig);
+		vector<float> v;
+		if (scheme == "1")
+		{
+			ligand lig;
+			lig.load(root + code + "/" + code + "_ligand.pdbqt");
+			v = feature(rec, lig);
+		}
+		else if (scheme == "5")
+		{
+			ifstream ifs(root + code + "/out/conformation_count.txt");
+			getline(ifs, line);
+			const size_t c = stoul(line);
+			vector<float> t;
+			v.resize((36 + 10) * 9 + 1);
+			size_t p = 0;
+			for (size_t i = c; i < 9; ++i)
+			{
+				ligand lig;
+				lig.load(root + code + "/out/" + code + "_ligand_ligand_1.pdbqt");
+				t = feature(rec, lig);
+				for (size_t j = 0; j < 36 + 10; ++j)
+				{
+					v[p++] = t[j];
+				}
+			}
+			for (size_t i = 1; i <= c; ++i)
+			{
+				ligand lig;
+				lig.load(root + code + "/out/" + code + "_ligand_ligand_" + to_string(i) + ".pdbqt");
+				t = feature(rec, lig);
+				for (size_t j = 0; j < 36 + 10; ++j)
+				{
+					v[p++] = t[j];
+				}
+			}
+			v[p++] = t.back();
+		}
+		else
+		{
+			ifstream ifs(root + code + "/vina-scheme-" + scheme + ".txt");
+			getline(ifs, line);
+			ligand lig;
+			lig.load(root + code + "/out/" + code + "_ligand_ligand_" + line + ".pdbqt");
+			v = feature(rec, lig);
+		}
 		ofs << pbindaff;
 		for (const auto d : v)
 		{
