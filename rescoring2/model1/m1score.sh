32ea67e28ed3cbc95da75fa155c3bda5a27fd06d
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-02-16 09:54:40 +0800

    Removed redundant ''

--- rescoring2/model1/m1score.sh
+++ rescoring2/model1/m1score.sh
@@ -1,4 +1,4 @@
 for tst in 1 2; do
-	tail -n +2 pdbbind-2007-trn-1-tst-$tst-iyp.csv | cut -d',' -f2,3 | rf-stat > pdbbind-2007-trn-1-tst-$tst-stat.csv
+	tail -n +2 pdbbind-2007-trn-1-tst-$tst-iyp.csv | cut -d, -f2,3 | rf-stat > pdbbind-2007-trn-1-tst-$tst-stat.csv
 	../../corplot.R 2007 1 $tst
 done
