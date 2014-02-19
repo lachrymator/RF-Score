0c51ee945313d33f2fe3eaa76788d5aa73e2456a
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-02-19 14:39:11 +0800

    Added code to generate tst-stat.csv in model3.sh for rescoring

--- rescoring/model3.sh
+++ rescoring/model3.sh
@@ -4,6 +4,7 @@ for m in 3 4 5; do
 	for s in 1 2; do
 		echo set$s
 		cd set$s
+		echo v,w,n,rmse,sdev,pcor,scor,kcor > tst-stat.csv
 		for v in $(ls -1 pdbbind-*-trn-yxi.csv | cut -d- -f2); do
 			echo $v
 			echo seed,n,rmse,sdev,pcor,scor,kcor > pdbbind-$v-tst-stat.csv
@@ -24,6 +25,8 @@ for m in 3 4 5; do
 				tail -1 $w/pdbbind-$v-tst-stat.csv >> pdbbind-$v-tst-stat.csv
 				tail -1 $w/pdbbind-$v-trn-stat.csv >> pdbbind-$v-trn-stat.csv
 			done
+			echo -n $v, >> tst-stat.csv
+			tail -n +2 pdbbind-$v-tst-stat.csv | head -1 >> tst-stat.csv
 		done
 		cd ..
 	done
