23d6285048d6d1c8b0f61b94ec3854383c76c923
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-01 16:58:16 +0800

    Added code to detect WARNING in log file

--- rescoring2/pdbbind.sh
+++ rescoring2/pdbbind.sh
@@ -10,17 +10,15 @@ for v in 2002 2007 2010 2012 2013; do
 		vina --config box.conf --receptor ${c}_protein.pdbqt --ligand ${c}_ligand.pdbqt --log log/${c}_ligand.txt --out out/${c}_ligand.pdbqt
 		~/idock/utilities/rmsd ${c}_ligand.pdbqt out/${c}_ligand.pdbqt > vina.rmsd
 		vina_split --input out/${c}_ligand.pdbqt
-#		ln -s ../${c}_ligand.pdbqt out/${c}_ligand_ligand_0.pdbqt
 		echo 0 > vina-scheme-1.txt
 		echo 1 > vina-scheme-2.txt
 		n=$(wc -l < vina.rmsd)
 		echo $n > out/conformation_count.txt
 		paste ../../seq$n vina.rmsd | sort -s -k2,2n | head -1 | cut -f1 > vina-scheme-3.txt
-		tail -n +26 log/${c}_ligand.txt | head -$n | awk -v y=$(cat pbindaff.txt) 'function abs(x){return x < 0 ? -x : x} {print abs(substr($0,13,5) * -0.73349480509 - y)}' | paste ../../seq$n - | sort -s -k2,2n | head -1 | cut -f1 > vina-scheme-4.txt
-		for i in {1..9}; do
-			if [[ -f out/${c}_ligand_ligand_${i}.pdbqt ]]; then
-				vina --receptor ${c}_protein.pdbqt --ligand out/${c}_ligand_ligand_${i}.pdbqt --score_only > out/${c}_ligand_ligand_${i}.txt
-			fi
+		w=$(grep "WARNING" log/${c}_ligand.txt | wc -l)
+		tail -n +$((26+$w)) log/${c}_ligand.txt | head -$n | awk -v y=$(cat pbindaff.txt) 'function abs(x){return x < 0 ? -x : x} {print abs(substr($0,13,5) * -0.73349480509 - y)}' | paste ../../seq$n - | sort -s -k2,2n | head -1 | cut -f1 > vina-scheme-4.txt
+		for i in $(seq 1 $n); do
+			vina --receptor ${c}_protein.pdbqt --ligand out/${c}_ligand_ligand_${i}.pdbqt --score_only > out/${c}_ligand_ligand_${i}.txt
 		done
 		cd ..
 	done
