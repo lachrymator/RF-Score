f4d29c28ca1849bebc952b09ba129183f42a6fcf
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-01 19:53:38 +0800

    Updated the echo message in model4prepare.sh to be more precise

--- rescoring2/model4prepare.sh
+++ rescoring2/model4prepare.sh
@@ -5,14 +5,14 @@ cd set1
 echo set1
 for v in 2007; do
 	for trn in 1 2 3 4 5 6; do
-		echo tst-$trn
+		echo tst-$trn-yxi.csv
 		rf-prepare ~/PDBbind/v$v/pdbbind-$v-core-iy.csv tst-$trn-yxi.csv $trn
 	done
 done
 for v in 2007; do
 	echo $v
 	for trn in 1 2 3 4 5 6; do
-		echo trn-$trn
+		echo pdbbind-$v-trn-$trn-yxi.csv
 		rf-prepare ~/PDBbind/v$v/pdbbind-$v-refined-core-iy.csv pdbbind-$v-trn-$trn-yxi.csv $trn
 	done
 done
@@ -21,14 +21,14 @@ cd set2
 echo set2
 for v in 2013; do
 	for trn in 1 2 3 4 5 6; do
-		echo tst-$trn
+		echo tst-$trn-yxi.csv
 		rf-prepare ~/PDBbind/v$v/rescoring2-iy.csv tst-$trn-yxi.csv $trn
 	done
 done
 for v in 2002 2007 2010 2012; do
 	echo $v
 	for trn in 1 2 3 4 5 6; do
-		echo trn-$trn
+		echo pdbbind-$v-trn-$trn-yxi.csv
 		rf-prepare ~/PDBbind/v$v/rescoring2-iy.csv pdbbind-$v-trn-$trn-yxi.csv $trn
 	done
 done
