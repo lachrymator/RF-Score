3765dbe87d7ba083aa15a69f19cc4b2fc5a0e66d
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-03 14:38:23 +0800

    Removed all cd's

--- rescoring2/rmsd.sh
+++ rescoring2/rmsd.sh
@@ -1,5 +1,4 @@
 prefix=~/PDBbind
-pwd=$(pwd)
 rmsdts=(0.5 1.0 1.5 2.0 2.5 3.0) # RMSD thresholds
 v=(2007 2013)
 c=(pdbbind-2007-core-i.csv rescoring2.csv)
@@ -15,18 +14,17 @@ for s in 1 2; do
 		rf-train model4/set$s/pdbbind-2007-trn-1-yxi.csv model4/set$s/$w4/pdbbind-2007-trn-1.rf $w4
 	fi
 	si=$((s-1))
-	cd $prefix/v${v[si]}
+	pv=$prefix/v${v[si]}
 	rmsd1s=(0 0 0 0 0 0)
 	rmsdms=(0 0 0 0 0 0)
 	rmsdis1=(0 0 0 0 0 0 0 0 0)
 	k=0
-	for c in $(< ${c[si]}); do
+	for c in $(< $pv/${c[si]}); do
 		k=$((k+1))
-#		echo $k $c
-		cd $c
+		pvc=$pv/$c
 		rmsdf=vina.rmsd
-		rmsd1=$(head -1 $rmsdf)
-		rmsdm=$(sort -n $rmsdf | head -1)
+		rmsd1=$(head -1 $pvc/$rmsdf)
+		rmsdm=$(sort -n $pvc/$rmsdf | head -1)
 		for i in $(seq 0 5); do
 			if [[ $(bc <<< "$rmsd1 < ${rmsdts[i]}") == 1 ]]; then
 				rmsd1s[i]=$((rmsd1s[i]+1))
@@ -35,27 +33,24 @@ for s in 1 2; do
 				rmsdms[i]=$((rmsdms[i]+1))
 			fi
 		done
-		s3=$(< vina-scheme-3.txt)
-		n=$(wc -l < vina.rmsd)
+		s3=$(< $pvc/vina-scheme-3.txt)
+		n=$(wc -l < $pvc/$rmsdf)
 #		rf-score dummy ../${c}_protein.pdbqt ${c}_ligand_ligand_1.pdbqt > model2-x.csv
 #		for i in $(seq 2 $n); do
 #			rf-score dummy ../${c}_protein.pdbqt ${c}_ligand_ligand_${i}.pdbqt | tail -1 >> model2-x.csv
 #		done
-		w=$(grep "WARNING" log/${c}_ligand.txt | wc -l)
-		p1=$(tail -n +$((26+w)) log/${c}_ligand.txt | head -$n | awk '{print substr($0,13,5) * -0.73349480509}')
-		cd out
+		w=$(grep "WARNING" $pvc/log/${c}_ligand.txt | wc -l)
+		p1=$(tail -n +$((26+w)) $pvc/log/${c}_ligand.txt | head -$n | awk '{print substr($0,13,5) * -0.73349480509}')
 #		../../mlrtest.R 2007 1 dummy $w2 dummy > model2-p.csv
 #		tail -n +2 model3-x.csv | rf-score model3/set$s/$w3/pdbbind-2007-trn-1.rf > model3-p.csv
 #		tail -n +2 model4-x.csv | rf-score model4/set$s/$w4/pdbbind-2007-trn-1.rf > model4-p.csv
 		i=0
-		for r in $(paste ../../../seq$n - <<< $p1 | sort -k2,2nr | cut -f1); do
+		for r in $(paste $prefix/seq$n - <<< $p1 | sort -k2,2nr | cut -f1); do
 			if [[ $s3 == $r ]]; then
 				rmsdis1[i]=$((rmsdis1[i]+1))
 			fi
 			i=$((i+1))
 		done
-		cd ..
-		cd ..
 	done
 	echo "condition,#,%"
 	for i in {0..8}; do
@@ -67,5 +62,4 @@ for s in 1 2; do
 	for i in $(seq 0 5); do
 		echo RMSDm"<"${rmsdts[i]},${rmsdms[i]},$(printf "%.0f\n" $(bc -l <<< "${rmsdms[i]} * 100 / $k"))%
 	done
-	cd $pwd
 done
