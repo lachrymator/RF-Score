5fbfea68f541a3223d72560569dcef462d919a5d
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-03 15:24:45 +0800

    Added comments

--- rescoring2/rmsd.sh
+++ rescoring2/rmsd.sh
@@ -1,10 +1,10 @@
 prefix=~/PDBbind
 rmsdts=(0.5 1.0 1.5 2.0 2.5 3.0) # RMSD thresholds
 v=(2007 2013)
-c=(pdbbind-2007-core-i.csv rescoring2.csv)
+c=(pdbbind-2007-core-i.csv rescoring2.csv) # rescoring-2-set-1-tst-iy.csv
 for s in 1 2; do
 	echo set$s
-	w2=$(tail -n +2 model2/set$s/tst-stat.csv | grep 2007,1,1, | cut -d, -f4)
+	w2=$(tail -n +2 model2/set$s/tst-stat.csv | grep 2007,1,1, | cut -d, -f4) # Always use PDBbind v2007 as training set, no matter what set is.
 	w3=$(tail -n +2 model3/set$s/tst-stat.csv | grep 2007,1,1, | cut -d, -f4)
 	if [[ ! -s model3/set$s/$w3/pdbbind-2007-trn-1.rf ]]; then
 		rf-train model3/set$s/pdbbind-2007-trn-1-yxi.csv model3/set$s/$w3/pdbbind-2007-trn-1.rf $w3
@@ -35,13 +35,13 @@ for s in 1 2; do
 		done
 		s3=$(< $pvc/vina-scheme-3.txt)
 		n=$(wc -l < $pvc/$rmsdf)
+		w=$(grep "WARNING" $pvc/log/${c}_ligand.txt | wc -l)
+		p1=$(tail -n +$((26+w)) $pvc/log/${c}_ligand.txt | head -$n | awk '{print substr($0,13,5) * -0.73349480509}')
 #		rf-score dummy ../${c}_protein.pdbqt ${c}_ligand_ligand_1.pdbqt > model2-x.csv
 #		for i in $(seq 2 $n); do
 #			rf-score dummy ../${c}_protein.pdbqt ${c}_ligand_ligand_${i}.pdbqt | tail -1 >> model2-x.csv
 #		done
-		w=$(grep "WARNING" $pvc/log/${c}_ligand.txt | wc -l)
-		p1=$(tail -n +$((26+w)) $pvc/log/${c}_ligand.txt | head -$n | awk '{print substr($0,13,5) * -0.73349480509}')
-#		../../mlrtest.R 2007 1 dummy $w2 dummy > model2-p.csv
+#		../../mlrtest.R 2007 1 dummy $w2 > model2-p.csv
 #		tail -n +2 model3-x.csv | rf-score model3/set$s/$w3/pdbbind-2007-trn-1.rf > model3-p.csv
 #		tail -n +2 model4-x.csv | rf-score model4/set$s/$w4/pdbbind-2007-trn-1.rf > model4-p.csv
 		i=0
