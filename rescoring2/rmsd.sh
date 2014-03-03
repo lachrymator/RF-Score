b77a25c9572b099b29619350dd5db01c32aba31b
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-03 16:30:24 +0800

    Added code to compute rmsdis4

--- rescoring2/rmsd.sh
+++ rescoring2/rmsd.sh
@@ -18,6 +18,7 @@ for s in 1 2; do
 	rmsd1s=(0 0 0 0 0 0)
 	rmsdms=(0 0 0 0 0 0)
 	rmsdis1=(0 0 0 0 0 0 0 0 0)
+	rmsdis4=(0 0 0 0 0 0 0 0 0)
 	k=0
 	for c in $(< $pv/${c[si]}); do
 		k=$((k+1))
@@ -25,7 +26,7 @@ for s in 1 2; do
 		rmsdf=vina.rmsd
 		rmsd1=$(head -1 $pvc/$rmsdf)
 		rmsdm=$(sort -n $pvc/$rmsdf | head -1)
-		for i in $(seq 0 5); do
+		for i in {0..5}; do
 			if [[ $(bc <<< "$rmsd1 < ${rmsdts[i]}") == 1 ]]; then
 				rmsd1s[i]=$((rmsd1s[i]+1))
 			fi
@@ -36,18 +37,25 @@ for s in 1 2; do
 		s3=$(< $pvc/vina-scheme-3.txt)
 		n=$(wc -l < $pvc/$rmsdf)
 		w=$(grep "WARNING" $pvc/log/${c}_ligand.txt | wc -l)
-		p1=$(tail -n +$((26+w)) $pvc/log/${c}_ligand.txt | head -$n | awk '{print substr($0,13,5) * -0.73349480509}')
-#		rf-extract dummy ../${c}_protein.pdbqt ${c}_ligand_ligand_1.pdbqt > x2.csv
-#		for i in $(seq 2 $n); do
-#			rf-extract dummy ../${c}_protein.pdbqt ${c}_ligand_ligand_${i}.pdbqt | tail -n +2 >> x2.csv
-#		done
-#		p2=$(../../mlrtest.R 2007 1 dummy $w2)
-#		p3=$(tail -n +2 x3.csv | rf-predict model3/set$s/$w3/pdbbind-2007-trn-1.rf)
-#		p4=$(tail -n +2 x4.csv | rf-predict model4/set$s/$w4/pdbbind-2007-trn-1.rf)
+		tail -n +$((26+w)) $pvc/log/${c}_ligand.txt | head -$n | awk '{print substr($0,13,5) * -0.73349480509}' > /tmp/p1.csv
+		rf-extract $pvc/${c}_protein.pdbqt $pvc/out/${c}_ligand_ligand_1.pdbqt > /tmp/x4.csv # Extract features and predict binding affinity values for model 4. Update src/feature.hpp before changing the model from 4 to 3 or 2.
+		for i in $(seq 2 $n); do
+			rf-extract $pvc/${c}_protein.pdbqt $pvc/out/${c}_ligand_ligand_${i}.pdbqt | tail -n +2 >> /tmp/x4.csv
+		done
+		tail -n +2 /tmp/x4.csv | rf-predict model4/set$s/$w4/pdbbind-2007-trn-1.rf > /tmp/p4.csv
+#		tail -n +2 /tmp/x3.csv | rf-predict model3/set$s/$w3/pdbbind-2007-trn-1.rf > /tmp/p3.csv
+#		mlrtestp.R 2007 1 $w2 > /tmp/p2.csv
 		i=0
-		for r in $(paste $prefix/seq$n - <<< $p1 | sort -k2,2nr | cut -f1); do
+		for r in $(paste $prefix/seq$n /tmp/p1.csv | sort -k2,2nr | cut -f1); do
 			if [[ $s3 == $r ]]; then
 				rmsdis1[i]=$((rmsdis1[i]+1))
+			fi
+			i=$((i+1))
+		done
+		i=0
+		for r in $(paste $prefix/seq$n /tmp/p4.csv | sort -k2,2nr | cut -f1); do
+			if [[ $s3 == $r ]]; then
+				rmsdis4[i]=$((rmsdis4[i]+1))
 			fi
 			i=$((i+1))
 		done
@@ -56,10 +64,13 @@ for s in 1 2; do
 	for i in {0..8}; do
 		echo "RMSD$[i+1]=RMSDm",${rmsdis1[i]},$(printf "%.0f\n" $(bc -l <<< "${rmsdis1[i]} * 100 / $k"))%
 	done
-	for i in $(seq 0 5); do
+	for i in {0..5}; do
 		echo RMSD1"<"${rmsdts[i]},${rmsd1s[i]},$(printf "%.0f\n" $(bc -l <<< "${rmsd1s[i]} * 100 / $k"))%
 	done
-	for i in $(seq 0 5); do
+	for i in {0..5}; do
 		echo RMSDm"<"${rmsdts[i]},${rmsdms[i]},$(printf "%.0f\n" $(bc -l <<< "${rmsdms[i]} * 100 / $k"))%
+	done
+	for i in {0..8}; do
+		echo "RMSD$[i+1]=RMSDm",${rmsdis4[i]},$(printf "%.0f\n" $(bc -l <<< "${rmsdis4[i]} * 100 / $k"))%
 	done
 done
