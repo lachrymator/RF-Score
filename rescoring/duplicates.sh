0c89d23d4268c5e6de435f79a96c24504708136e
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-04 11:28:52 +0800

    Added duplicates.sh for rescoring and rescoring2

---
+++ rescoring/duplicates.sh
@@ -0,0 +1,21 @@
+prefix=~/PDBbind
+declare -A v
+v[1,0]=2007
+v[1,1]=2004
+v[1,2]=2007
+v[1,3]=2010
+v[1,4]=2013
+v[2,0]=2013
+v[2,1]=2002
+v[2,2]=2007
+v[2,3]=2010
+v[2,4]=2012
+t=(tst trn trn trn trn)
+for s in 1 2; do
+	echo set$s
+	for v0 in {0..3}; do
+	for v1 in $(seq $((v0+1)) 4); do
+		echo "|$v0 ∩ $v1| = "$(cut -d, -f1 $prefix/v${v[$s,$v0]}/rescoring-1-set-$s-${t[v0]}-iy.csv $prefix/v${v[$s,$v1]}/rescoring-1-set-$s-${t[v1]}-iy.csv | sort | uniq -d | wc -l)
+	done
+	done
+done
