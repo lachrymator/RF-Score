620caf83d72b4b148f5491257d69f72361611394
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-04 13:45:59 +0800

    Used pdbbind-2007-refined-iy.csv instead of rescoring-2-set-2-trn-iy.csv in pdbbind.sh

--- rescoring2/pdbbind.sh
+++ rescoring2/pdbbind.sh
@@ -1,6 +1,6 @@
-for v in 2002 2007 2010 2012 2013; do
+for v in 2002 2004 2007 2010 2012 2013; do
 	cd v$v
-	for iy in $(cat rescoring-2-set-2-trn-iy.csv); do
+	for iy in $(cat pdbbind-$v-refined-iy.csv); do
 		c=${iy:0:4}
 		echo $v $c
 		cd $c
@@ -18,7 +18,8 @@ for v in 2002 2007 2010 2012 2013; do
 		paste ../../seq$n vina.rmsd | sort -s -k2,2n | head -1 | cut -f1 > vina-scheme-3.txt
 		w=$(grep "WARNING" log/${c}_ligand.txt | wc -l)
 		tail -n +$((26+$w)) log/${c}_ligand.txt | head -$n | awk -v y=$(cat pbindaff.txt) 'function abs(x){return x < 0 ? -x : x} {print abs(substr($0,13,5) * -0.73349480509 - y)}' | paste ../../seq$n - | sort -s -k2,2n | head -1 | cut -f1 > vina-scheme-4.txt
-		for i in $(seq 1 $n); do
+		ln -s ../${c}_ligand.pdbqt out/${c}_ligand_ligand_0.pdbqt
+		for i in {0..1}; do
 			vina --receptor ${c}_protein.pdbqt --ligand out/${c}_ligand_ligand_${i}.pdbqt --score_only > out/${c}_ligand_ligand_${i}.txt
 		done
 		cd ..
