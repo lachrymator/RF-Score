e33a5d78b9fffcd2a877ce758476187de71535da
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-04 12:42:38 +0800

    Combined set1 and set2

--- rescoring2/model1prepare.sh
+++ rescoring2/model1prepare.sh
@@ -1,35 +1,23 @@
 prefix=~/PDBbind
+v=(dummy 2007 2013)
 m=1
 cd model$m
 echo model$m
-cd set1
-echo set1
-tail -n +2 ../../set1/tst-1-iy.csv > tst-1-iy.csv
-echo PDB,pbindaff,predicted > pdbbind-2007-trn-1-tst-1-iyp.csv
-for iy in $(cat $prefix/v2007/rescoring-2-set-1-tst-iy.csv); do
-	c=${iy:0:4}
-	tail -n +18 $prefix/v2007/${c}/vina_score.txt | head -1
-done | cut -d' ' -f2 | awk '{printf "%.2f\n", $1*-0.73349480509}' | paste -d, tst-1-iy.csv - >> pdbbind-2007-trn-1-tst-1-iyp.csv
-echo PDB,pbindaff,predicted > pdbbind-2007-trn-1-tst-2-iyp.csv
-for iy in $(cat $prefix/v2007/rescoring-2-set-1-tst-iy.csv); do
-	c=${iy:0:4}
-	tail -n +18 $prefix/v2007/${c}/out/${c}_ligand_ligand_1.txt | head -1
-done | cut -d' ' -f2 | awk '{printf "%.2f\n", $1*-0.73349480509}' | paste -d, tst-1-iy.csv - >> pdbbind-2007-trn-1-tst-2-iyp.csv
-rm tst-1-iy.csv
-cd ..
-cd set2
-echo set2
-tail -n +2 ../../set2/tst-1-iy.csv > tst-1-iy.csv
-echo PDB,pbindaff,predicted > pdbbind-2007-trn-1-tst-1-iyp.csv
-for iy in $(cat $prefix/v2013/rescoring-2-set-2-tst-iy.csv); do
-	c=${iy:0:4}
-	tail -n +18 $prefix/v2013/${c}/vina_score.txt | head -1
-done | cut -d' ' -f2 | awk '{printf "%.2f\n", $1*-0.73349480509}' | paste -d, tst-1-iy.csv - >> pdbbind-2007-trn-1-tst-1-iyp.csv
-echo PDB,pbindaff,predicted > pdbbind-2007-trn-1-tst-2-iyp.csv
-for iy in $(cat $prefix/v2013/rescoring-2-set-2-tst-iy.csv); do
-	c=${iy:0:4}
-	tail -n +18 $prefix/v2013/${c}/out/${c}_ligand_ligand_1.txt | head -1
-done | cut -d' ' -f2 | awk '{printf "%.2f\n", $1*-0.73349480509}' | paste -d, tst-1-iy.csv - >> pdbbind-2007-trn-1-tst-2-iyp.csv
-rm tst-1-iy.csv
-cd ..
+for s in 1 2; do
+	cd set$s
+	echo set$s
+	tail -n +2 ../../set$s/tst-1-iy.csv > tst-1-iy.csv
+	echo PDB,pbindaff,predicted > pdbbind-2007-trn-1-tst-1-iyp.csv
+	for iy in $(cat $prefix/v${v[s]}/rescoring-2-set-$s-tst-iy.csv); do
+		c=${iy:0:4}
+		tail -n +18 $prefix/v${v[s]}/${c}/vina_score.txt | head -1
+	done | cut -d' ' -f2 | awk '{printf "%.2f\n", $1*-0.73349480509}' | paste -d, tst-1-iy.csv - >> pdbbind-2007-trn-1-tst-1-iyp.csv
+	echo PDB,pbindaff,predicted > pdbbind-2007-trn-1-tst-2-iyp.csv
+	for iy in $(cat $prefix/v${v[s]}/rescoring-2-set-$s-tst-iy.csv); do
+		c=${iy:0:4}
+		tail -n +18 $prefix/v${v[s]}/${c}/out/${c}_ligand_ligand_1.txt | head -1
+	done | cut -d' ' -f2 | awk '{printf "%.2f\n", $1*-0.73349480509}' | paste -d, tst-1-iy.csv - >> pdbbind-2007-trn-1-tst-2-iyp.csv
+	rm tst-1-iy.csv
+	cd ..
+done
 cd ..
