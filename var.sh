7666502e7d416d7da499c887605fa7b9bdfad64c
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-11 20:35:41 +0800

    Added var.sh

---
+++ var.sh
@@ -0,0 +1,3 @@
+rf-train pdbbind-2007-refined-core-yx${1}i.csv pdbbind-2007-refined-core-x${1}.rf
+rf-test pdbbind-2007-refined-core-x${1}.rf pdbbind-2007-refined-core-yx${1}i.csv iyp.csv
+rf-test pdbbind-2007-refined-core-x${1}.rf pdbbind-2007-core-yx${1}i.csv iyp.csv
