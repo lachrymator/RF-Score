352ac91886fa68adb7810ba651402ebc2027376c
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-14 11:14:45 +0800

    Added a condition

--- rescoring/model1.sh
+++ rescoring/model1.sh
@@ -4,7 +4,9 @@ for m in 1; do
 	for s in 1 2; do
 		echo set$s
 		cd set$s
-#		tail -n +2 pdbbind-2007-trn-iyp.csv | cut -d, -f2,3 | rf-stat > pdbbind-2007-trn-stat.csv
+		if [[ $s == 1 ]]; then
+			tail -n +2 pdbbind-2007-trn-iyp.csv | cut -d, -f2,3 | rf-stat > pdbbind-2007-trn-stat.csv
+		fi
 		tail -n +2 pdbbind-2007-tst-iyp.csv | cut -d, -f2,3 | rf-stat > pdbbind-2007-tst-stat.csv
 		../../iypplot.R 2007
 		cd ..
