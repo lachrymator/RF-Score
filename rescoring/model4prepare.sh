8fa00d91ec9a8fea02e4b06c69d3fbf4bd13efdc
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-15 22:55:20 +0800

    Made m loop over 4

--- rescoring/model4prepare.sh
+++ rescoring/model4prepare.sh
@@ -10,21 +10,22 @@ v[2,1]=2002
 v[2,2]=2007
 v[2,3]=2010
 v[2,4]=2012
-m=4 # Update src/rf-prepare.cpp and src/feature.cpp before changing m from 4 to 3 or 2.
-cd model$m
-echo model$m
-for s in 1 2; do
-	cd set$s
-	echo set$s
-	for vi in {0..4}; do
-		if [[ $vi == 0 ]]; then
-			echo tst-yxi.csv
-			rf-prepare $pdbbind/v${v[$s,$vi]}/rescoring-1-set-$s-tst-iy.csv > tst-yxi.csv
-		else
-			echo pdbbind-${v[$s,$vi]}-trn-yxi.csv
-			rf-prepare $pdbbind/v${v[$s,$vi]}/rescoring-1-set-$s-trn-iy.csv > pdbbind-${v[$s,$vi]}-trn-yxi.csv
-		fi
+for m in 4; do # Update src/rf-prepare.cpp and src/feature.cpp before changing m from 4 to 3 or 2.
+	cd model$m
+	echo model$m
+	for s in 1 2; do
+		cd set$s
+		echo set$s
+		for vi in {0..4}; do
+			if [[ $vi == 0 ]]; then
+				echo tst-yxi.csv
+				rf-prepare $pdbbind/v${v[$s,$vi]}/rescoring-1-set-$s-tst-iy.csv > tst-yxi.csv
+			else
+				echo pdbbind-${v[$s,$vi]}-trn-yxi.csv
+				rf-prepare $pdbbind/v${v[$s,$vi]}/rescoring-1-set-$s-trn-iy.csv > pdbbind-${v[$s,$vi]}-trn-yxi.csv
+			fi
+		done
+		cd ..
 	done
 	cd ..
 done
-cd ..
--- rescoring2/model4prepare.sh
+++ rescoring2/model4prepare.sh
@@ -10,23 +10,24 @@ v[2,1]=2002
 v[2,2]=2007
 v[2,3]=2010
 v[2,4]=2012
-m=4 # Update src/rf-prepare.cpp and src/feature.cpp before changing m from 4 to 3 or 2.
-cd model$m
-echo model$m
-for s in 1 2; do
-	cd set$s
-	echo set$s
-	for vi in {0..4}; do
-		for trn in {1..6}; do
-			if [[ $vi == 0 ]]; then
-				echo tst-$trn-yxi.csv
-				rf-prepare $pdbbind/v${v[$s,$vi]}/rescoring-2-set-$s-tst-iy.csv $trn > tst-$trn-yxi.csv
-			else
-				echo pdbbind-${v[$s,$vi]}-trn-$trn-yxi.csv
-				rf-prepare $pdbbind/v${v[$s,$vi]}/rescoring-2-set-$s-trn-iy.csv $trn > pdbbind-${v[$s,$vi]}-trn-$trn-yxi.csv
-			fi
+for m in 4; do # Update src/rf-prepare.cpp and src/feature.cpp before changing m from 4 to 3 or 2.
+	cd model$m
+	echo model$m
+	for s in 1 2; do
+		cd set$s
+		echo set$s
+		for vi in {0..4}; do
+			for trn in {1..6}; do
+				if [[ $vi == 0 ]]; then
+					echo tst-$trn-yxi.csv
+					rf-prepare $pdbbind/v${v[$s,$vi]}/rescoring-2-set-$s-tst-iy.csv $trn > tst-$trn-yxi.csv
+				else
+					echo pdbbind-${v[$s,$vi]}-trn-$trn-yxi.csv
+					rf-prepare $pdbbind/v${v[$s,$vi]}/rescoring-2-set-$s-trn-iy.csv $trn > pdbbind-${v[$s,$vi]}-trn-$trn-yxi.csv
+				fi
+			done
 		done
+		cd ..
 	done
 	cd ..
 done
-cd ..
