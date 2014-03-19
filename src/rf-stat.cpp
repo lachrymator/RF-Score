2fb6aba5141b59235847d48324724e6157566e83
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-19 22:24:55 +0800

    Swapped p and y in rf-stat.cpp

--- src/rf-stat.cpp
+++ src/rf-stat.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	vector<float> p, y;
-	p.reserve(100);
+	vector<float> y, p;
 	y.reserve(100);
+	p.reserve(100);
 	for (string line; getline(cin, line);)
 	{
 		size_t c = line.find(',');
-		p.push_back(stof(line.substr(0, c)));
-		y.push_back(stof(line.substr(c + 1)));
+		y.push_back(stof(line.substr(0, c)));
+		p.push_back(stof(line.substr(c + 1)));
 	}
-	const auto s = stats(p, y);
+	const auto s = stats(y, p);
 
 	cout.setf(ios::fixed, ios::floatfield);
 	cout << "n,rmse,sdev,pcor,scor,kcor" << endl << y.size() << ',' << setprecision(3) << s[0] << ',' << s[1] << ',' << s[2] << ',' << s[3] << ',' << s[4] << endl;
