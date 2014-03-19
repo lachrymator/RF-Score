232d12809260ad783ab75b29cdafa34894c3f715
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-19 23:52:05 +0800

    Renamed *-iyp.tiff to *-iypr.tiff in iypplot.R

--- rescoring/iypplot.R
+++ rescoring/iypplot.R
@@ -16,7 +16,7 @@ kcor=cor(d["regressed"], d["pbindaff"], method="kendall")
 cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor), file=sprintf("pdbbind-%s-%s-stat.csv",v,t))
 xylim=c(2,14) # Set1's range.
 if (n == 382) xylim=c(1,12) # Set2's range.
-tiff(sprintf("pdbbind-%s-%s-iyr.tiff",v,t),compress="lzw")
+tiff(sprintf("pdbbind-%s-%s-iypr.tiff",v,t),compress="lzw")
 par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
 plot(d[,"pbindaff"], d[,"regressed"], xlim=xylim, ylim=xylim, xlab="Measured binding affinity (pKd)", ylab="Predicted binding affinity (pKd)", main=sprintf("N=%d, RMSE=%.2f, SD=%.2f, Rp=%.3f, Rs=%.3f", n, rmse, sdev, pcor, scor))
 abline(lm(d[,"regressed"]~d[,"pbindaff"]))
