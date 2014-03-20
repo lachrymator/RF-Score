514b42bb31a633b0a9c44002f10c2e94c633d20a
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-20 16:54:07 +0800

    Changed the idp variable to d

--- rescoring2/idpplot.R
+++ rescoring2/idpplot.R
@@ -3,13 +3,13 @@ args=commandArgs(trailingOnly=T)
 v=args[1]
 trn=args[2]
 tst=args[3]
-idp=read.csv(sprintf("pdbbind-%s-trn-%s-tst-%s-idp.csv",v,trn,tst))
-n=nrow(idp)
+d=read.csv(sprintf("pdbbind-%s-trn-%s-tst-%s-idp.csv",v,trn,tst))
+n=nrow(d)
 ylim=c(2,14) # Set1's range.
 if (n == 382) ylim=c(1,12) # Set2's range.
 tiff(sprintf("pdbbind-%s-trn-%s-tst-%s-idp.tiff",v,trn,tst),compress="lzw")
 par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
-plot(idp[,"RMSD1"], idp[,"predicted"], xlim=c(0,15), ylim=ylim, xlab="RMSD (Å)", ylab="Predicted binding affinity (pKd)", main=sprintf("N=%d", n))
+plot(d[,"RMSD1"], d[,"predicted"], xlim=c(0,15), ylim=ylim, xlab="RMSD (Å)", ylab="Predicted binding affinity (pKd)", main=sprintf("N=%d", n))
 axis(1,at=2)
 abline(v=2,lty=3)
 grid()
