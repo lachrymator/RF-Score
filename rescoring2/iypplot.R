f55f11d6558c4d24928b8fa5087bc25380e1135b
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-20 14:08:45 +0800

    Renamed the iyp variable to d

--- rescoring2/iypplot.R
+++ rescoring2/iypplot.R
@@ -3,18 +3,18 @@ args=commandArgs(trailingOnly=T)
 v=args[1]
 trn=args[2]
 tst=args[3]
-iyp=read.csv(sprintf("pdbbind-%s-trn-%s-tst-%s-iyp.csv",v,trn,tst),check.names=F)
-n=nrow(iyp)
-se=sum((iyp[3] - iyp[2])^2)
+d=read.csv(sprintf("pdbbind-%s-trn-%s-tst-%s-iyp.csv",v,trn,tst),check.names=F)
+n=nrow(d)
+se=sum((d[3] - d[2])^2)
 rmse=sqrt(se/n)
 sdev=sqrt(se/(n-1))
-pcor=cor(iyp[3], iyp[2], method="pearson")
-scor=cor(iyp[3], iyp[2], method="spearman")
-kcor=cor(iyp[3], iyp[2], method="kendall")
+pcor=cor(d[3], d[2], method="pearson")
+scor=cor(d[3], d[2], method="spearman")
+kcor=cor(d[3], d[2], method="kendall")
 xylim=c(2,14) # Set1's range.
 if (n == 382) xylim=c(1,12) # Set2's range.
 tiff(sprintf("pdbbind-%s-trn-%s-tst-%s-iyp.tiff",v,trn,tst),compress="lzw")
 par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
-plot(iyp[,2], iyp[,3], xlim=xylim, ylim=xylim, xlab="Measured binding affinity (pKd)", ylab="Predicted binding affinity (pKd)", main=sprintf("N=%d, RMSE=%.2f, SD=%.2f, Rp=%.3f, Rs=%.3f", n, rmse, sdev, pcor, scor))
-abline(lm(iyp[,3] ~ iyp[,2]))
+plot(d[,2], d[,3], xlim=xylim, ylim=xylim, xlab="Measured binding affinity (pKd)", ylab="Predicted binding affinity (pKd)", main=sprintf("N=%d, RMSE=%.2f, SD=%.2f, Rp=%.3f, Rs=%.3f", n, rmse, sdev, pcor, scor))
+abline(lm(d[,3] ~ d[,2]))
 grid()
