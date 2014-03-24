1e6a0f6d9fb2b7b9453d725e472219c491aae831
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-24 10:23:22 +0800

    Updated iyprplot.R to plot *-yp.tiff and *-yr.tiff

--- rescoring2/iyprplot.R
+++ rescoring2/iyprplot.R
@@ -8,17 +8,24 @@ r=lm(pbindaff~predicted,d)
 d["regressed"]=fitted(r)
 write.csv(c(d["PDB"],d["pbindaff"],d["predicted"],round(d["regressed"],2)),row.names=F,quote=F,file=sprintf("pdbbind-%s-trn-%s-tst-%s-iypr.csv",v,trn,tst))
 n=nrow(d) # Number of samples.
-se=sum((d["regressed"] - d["pbindaff"])^2) # Square error.
-rmse=sqrt(se/n)
-sdev=sqrt(se/(n-2))
-pcor=cor(d["regressed"], d["pbindaff"], method="pearson")
-scor=cor(d["regressed"], d["pbindaff"], method="spearman")
-kcor=cor(d["regressed"], d["pbindaff"], method="kendall")
-cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor), file=sprintf("pdbbind-%s-trn-%s-tst-%s-stat.csv",v,trn,tst))
 xylim=c(2,14) # Set1's range.
 if (n == 382) xylim=c(1,12) # Set2's range.
-tiff(sprintf("pdbbind-%s-trn-%s-tst-%s-iypr.tiff",v,trn,tst),compress="lzw")
-par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
-plot(d[,"pbindaff"], d[,"regressed"], xlim=xylim, ylim=xylim, xlab="Measured binding affinity (pKd)", ylab="Predicted binding affinity (pKd)", main=sprintf("N=%d, RMSE=%.2f, SD=%.2f, Rp=%.3f, Rs=%.3f", n, rmse, sdev, pcor, scor))
-abline(lm(d[,"regressed"] ~ d[,"pbindaff"]))
-grid()
+for (p in c("predicted","regressed"))
+{
+	l=substr(p,1,1)
+	se=sum((d[p] - d["pbindaff"])^2) # Square error.
+	rmse=sqrt(se/n)
+	sdev=sqrt(se/(n-2))
+	pcor=cor(d[p], d["pbindaff"], method="pearson")
+	scor=cor(d[p], d["pbindaff"], method="spearman")
+	kcor=cor(d[p], d["pbindaff"], method="kendall")
+	if (p == "regressed")
+	{
+		cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor), file=sprintf("pdbbind-%s-trn-%s-tst-%s-stat.csv",v,trn,tst))
+	}
+	tiff(sprintf("pdbbind-%s-trn-%s-tst-%s-y%s.tiff",v,trn,tst,l),compress="lzw")
+	par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
+	plot(d[,"pbindaff"], d[,p], xlim=xylim, ylim=xylim, xlab="Measured binding affinity (pKd)", ylab="Predicted binding affinity (pKd)", main=sprintf("N=%d, RMSE=%.2f, SD=%.2f, Rp=%.3f, Rs=%.3f", n, rmse, sdev, pcor, scor))
+	abline(lm(d[,p] ~ d[,"pbindaff"]))
+	grid()
+}
