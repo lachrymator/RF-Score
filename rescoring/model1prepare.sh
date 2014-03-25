6805c253c54a6d806cf5fe33020517a5b22412fa
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-25 10:38:32 +0800

    Updated model1preparep.sh to loop m over 1 instead of m=1

--- rescoring/model1prepare.sh
+++ rescoring/model1prepare.sh
@@ -1,18 +1,19 @@
 pdbbind=~/PDBbind
 v=(dummy 2007 2013)
-m=1
-cd model$m
-echo model$m
-for s in 1 2; do
-	cd set$s
-	echo set$s
-	tail -n +2 ../../set$s/tst-iy.csv > tst-iy.csv
-	echo PDB,pbindaff,predicted > pdbbind-2007-tst-iyp.csv
-	for iy in $(cat $pdbbind/v${v[s]}/rescoring-1-set-$s-tst-iy.csv); do
-		c=${iy:0:4}
-		tail -n +18 $pdbbind/v${v[s]}/${c}/out/${c}_ligand_ligand_0.txt | head -1
-	done | cut -d' ' -f2 | awk '{printf "%.2f\n", $1*-0.73349480509}' | paste -d, tst-iy.csv - >> pdbbind-2007-tst-iyp.csv
-	rm tst-iy.csv
+for m in 1; do
+	cd model$m
+	echo model$m
+	for s in 1 2; do
+		cd set$s
+		echo set$s
+		tail -n +2 ../../set$s/tst-iy.csv > tst-iy.csv
+		echo PDB,pbindaff,predicted > pdbbind-2007-tst-iyp.csv
+		for iy in $(cat $pdbbind/v${v[s]}/rescoring-1-set-$s-tst-iy.csv); do
+			c=${iy:0:4}
+			tail -n +18 $pdbbind/v${v[s]}/${c}/out/${c}_ligand_ligand_0.txt | head -1
+		done | cut -d' ' -f2 | awk '{printf "%.2f\n", $1*-0.73349480509}' | paste -d, tst-iy.csv - >> pdbbind-2007-tst-iyp.csv
+		rm tst-iy.csv
+		cd ..
+	done
 	cd ..
 done
-cd ..
