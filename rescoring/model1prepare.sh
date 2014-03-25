0429bd3636e3d60d092fc0fb04a8ead45edf6c91
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-25 10:47:15 +0800

    Updated model1prepare.sh to create pdbbind-2007-trn-iyp.csv for set 1

--- rescoring/model1prepare.sh
+++ rescoring/model1prepare.sh
@@ -6,13 +6,15 @@ for m in 1; do
 	for s in 1 2; do
 		cd set$s
 		echo set$s
-		tail -n +2 ../../set$s/tst-iy.csv > tst-iy.csv
-		echo PDB,pbindaff,predicted > pdbbind-2007-tst-iyp.csv
-		for iy in $(cat $pdbbind/v${v[s]}/rescoring-1-set-$s-tst-iy.csv); do
-			c=${iy:0:4}
-			tail -n +18 $pdbbind/v${v[s]}/${c}/out/${c}_ligand_ligand_0.txt | head -1
-		done | cut -d' ' -f2 | awk '{printf "%.2f\n", $1*-0.73349480509}' | paste -d, tst-iy.csv - >> pdbbind-2007-tst-iyp.csv
-		rm tst-iy.csv
+		for t in $([[ $s == 1 ]] && echo "tst trn" || echo "tst"); do
+			tail -n +2 ../../set$s/$t-iy.csv > $t-iy.csv
+			echo PDB,pbindaff,predicted > pdbbind-2007-$t-iyp.csv
+			for iy in $(cat $pdbbind/v${v[s]}/rescoring-1-set-$s-$t-iy.csv); do
+				c=${iy:0:4}
+				tail -n +18 $pdbbind/v${v[s]}/${c}/out/${c}_ligand_ligand_0.txt | head -1
+			done | cut -d' ' -f2 | awk '{printf "%.2f\n", $1*-0.73349480509}' | paste -d, $t-iy.csv - >> pdbbind-2007-$t-iyp.csv
+			rm $t-iy.csv
+		done
 		cd ..
 	done
 	cd ..
