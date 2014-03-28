04f74431ae841d565d3dcd113a20c4ef24c6c2c0
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-28 16:27:40 +0800

    Revised the usage of rf-score

--- src/rf-score.cpp
+++ src/rf-score.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
 {
 	if (argc != 4)
 	{
-		cout << "Usage: rf-score pdbbind-2012-refined-core-x42.rf receptor.pdbqt ligand.pdbqt" << endl;
+		cout << "Usage: rf-score pdbbind-2012-refined-core-x47.rf receptor.pdbqt ligand.pdbqt" << endl;
 		return 0;
 	}
 
