3fa7de858f23fe56b9290f2f7da0c2e5ee28de32
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-30 17:37:21 +0800

    Added x10

---
+++ cyscore/x10/mlrtest.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+args=commandArgs(trailingOnly=T)
+ntrn=args[1]
+ntst=args[2]
+c=read.csv(sprintf("trn-%s-coef.csv",ntrn))[,2]
+d=read.csv(sprintf("../tst-%s-yxi.csv",ntst))
+d["predicted"]=c[2]*d[2]+c[3]*d[3]+c[4]*d[4]+c[5]*d[5]+c[6]*d[6]+c[7]*d[7]+c[8]*d[8]+c[9]*d[9]+c[10]*d[10]+c[11]*d[11]
+d["regressed"]=fitted(lm(pbindaff~predicted,d))
+write.csv(c(d["PDB"],d["pbindaff"],round(d["predicted"],2)),row.names=F,quote=F,file=sprintf("trn-%s-tst-%s-iyp.csv",ntrn,ntst))
---
+++ cyscore/x10/mlrtrain.R
@@ -0,0 +1,6 @@
+#!/usr/bin/env Rscript
+args=commandArgs(trailingOnly=T)
+ntrn=args[1]
+trn=read.csv(sprintf("../trn-%s-yxi.csv",ntrn))
+c=coefficients(lm(pbindaff~Hydrophobic+Vdw+HBond+Ent+gauss1+gauss2+repulsion+hydrophobic+hydrogenbonding+flexibility,trn))
+write.csv(c,quote=F,file=sprintf("trn-%s-coef.csv",ntrn))
