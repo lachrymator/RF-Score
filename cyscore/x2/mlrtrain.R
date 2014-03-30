92df8e6ed61285131a23d36821c19d5fc5d2116c
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-30 20:29:11 +0800

    Added c[is.na(c)]=0 to mlrtrain.R

--- cyscore/x2/mlrtrain.R
+++ cyscore/x2/mlrtrain.R
@@ -3,4 +3,5 @@ args=commandArgs(trailingOnly=T)
 ntrn=args[1]
 trn=read.csv(sprintf("../trn-%s-yxi.csv",ntrn))
 c=coefficients(lm(pbindaff~Hydrophobic+Ent,trn))
+c[is.na(c)]=0
 write.csv(c,quote=F,file=sprintf("trn-%s-coef.csv",ntrn))
