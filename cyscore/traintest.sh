86ddd533318fd6cda2ee48c7de647968ebde6df5
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-30 09:39:19 +0800

    Echoed one -stat.csv at a line

--- cyscore/traintest.sh
+++ cyscore/traintest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ntsts=$(echo 195 201 382)
 ntrns=$(echo 247 1105 2280)
-echo x,model,ntrn,n,rmse,sdev,pcor,scor,kcor,n,rmse,sdev,pcor,scor,kcor,n,rmse,sdev,pcor,scor,kcor
+echo x,model,ntrn,ntst,rmse,sdev,pcor,scor,kcor
 for x in 2 4; do
 	cd x$x
 	cd mlr
@@ -9,8 +9,8 @@ for x in 2 4; do
 		./mlrtrain.R $ntrn
 		for ntst in $ntsts; do
 			./mlrtest.R $ntrn $ntst
+			echo $x,MLR,$ntrn,$(tail -1 trn-$ntrn-tst-$ntst-stat.csv)
 		done
-		echo $x,MLR,$ntrn,$(tail -1 trn-$ntrn-tst-195-stat.csv),$(tail -1 trn-$ntrn-tst-201-stat.csv),$(tail -1 trn-$ntrn-tst-382-stat.csv)
 	done
 	cd ..
 	cd rf
@@ -19,8 +19,8 @@ for x in 2 4; do
 		for ntst in $ntsts; do
 			rf-test trn-$ntrn.rf ../tst-$ntst-yxi.csv > trn-$ntrn-tst-$ntst-iyp.csv
 			tail -n +2 trn-$ntrn-tst-$ntst-iyp.csv | cut -d, -f2,3 | rf-stat > trn-$ntrn-tst-$ntst-stat.csv
+			echo $x,RF,$ntrn,$(tail -1 trn-$ntrn-tst-$ntst-stat.csv)
 		done
-		echo $x,RF,$ntrn,$(tail -1 trn-$ntrn-tst-195-stat.csv),$(tail -1 trn-$ntrn-tst-201-stat.csv),$(tail -1 trn-$ntrn-tst-382-stat.csv)
 	done
 	cd ..
 	cd ..
