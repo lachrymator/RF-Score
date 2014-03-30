15712378dc83de438a101d6ed2a0081b48b223d2
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-30 09:36:42 +0800

    Looped ntst over $ntsts in traintest.sh instead of mlrtest.R

--- cyscore/x2/mlr/mlrtest.R
+++ cyscore/x2/mlr/mlrtest.R
@@ -1,19 +1,17 @@
 #!/usr/bin/env Rscript
 args=commandArgs(trailingOnly=T)
 ntrn=args[1]
+ntst=args[2]
 c=read.csv(sprintf("trn-%s-coef.csv",ntrn))[,2]
-for (ntst in c(195,201,382))
-{
-	d=read.csv(sprintf("../tst-%s-yxi.csv",ntst))
-	d["predicted"]=c[2]*d[2]+c[3]*d[3]
-	d["regressed"]=fitted(lm(pbindaff~predicted,d))
-	write.csv(c(d["PDB"],d["pbindaff"],d["predicted"],d["regressed"]),row.names=F,quote=F,file=sprintf("trn-%s-tst-%s-iypr.csv",ntrn,ntst))
-	n=nrow(d)
-	se=sum((d["regressed"] - d["pbindaff"])^2)
-	rmse=sqrt(se/n)
-	sdev=sqrt(se/(n-2))
-	pcor=cor(d["regressed"], d["pbindaff"], method="pearson")
-	scor=cor(d["regressed"], d["pbindaff"], method="spearman")
-	kcor=cor(d["regressed"], d["pbindaff"], method="kendall")
-	cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor),file=sprintf("trn-%s-tst-%s-stat.csv",ntrn,ntst))
-}
+d=read.csv(sprintf("../tst-%s-yxi.csv",ntst))
+d["predicted"]=c[2]*d[2]+c[3]*d[3]
+d["regressed"]=fitted(lm(pbindaff~predicted,d))
+write.csv(c(d["PDB"],d["pbindaff"],d["predicted"],d["regressed"]),row.names=F,quote=F,file=sprintf("trn-%s-tst-%s-iypr.csv",ntrn,ntst))
+n=nrow(d)
+se=sum((d["regressed"] - d["pbindaff"])^2)
+rmse=sqrt(se/n)
+sdev=sqrt(se/(n-2))
+pcor=cor(d["regressed"], d["pbindaff"], method="pearson")
+scor=cor(d["regressed"], d["pbindaff"], method="spearman")
+kcor=cor(d["regressed"], d["pbindaff"], method="kendall")
+cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor),file=sprintf("trn-%s-tst-%s-stat.csv",ntrn,ntst))
