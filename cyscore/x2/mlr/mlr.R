389385401c3bff2d8534b5b9c83b527d25d1636e
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-30 09:08:50 +0800

    Used the number of complexes to rename *-iy.csv and *-yxi.csv

--- cyscore/x2/mlr/mlr.R
+++ cyscore/x2/mlr/mlr.R
@@ -1,14 +1,14 @@
 #!/usr/bin/env Rscript
 args=commandArgs(trailingOnly=T)
-v=args[1]
-trn=read.csv(sprintf("../../pdbbind-%s-trn-yxi.csv",v))
+ntrn=args[1]
+trn=read.csv(sprintf("../../trn-%s-yxi.csv",ntrn))
 c=coefficients(lm(pbindaff~Hydrophobic+Ent,trn))
-for (v in c(2007,2012,2013))
+for (ntst in c(195,201,382))
 {
-	d=read.csv(sprintf("../../pdbbind-%s-tst-yxi.csv",v))
+	d=read.csv(sprintf("../../tst-%s-yxi.csv",ntst))
 	d["predicted"]=c[2]*d[2]+c[3]*d[3]
 	d["regressed"]=fitted(lm(pbindaff~predicted,d))
-	write.csv(c(d["PDB"],d["pbindaff"],d["predicted"],d["regressed"]),row.names=F,quote=F,file=sprintf("pdbbind-%s-tst-iypr.csv",v))
+	write.csv(c(d["PDB"],d["pbindaff"],d["predicted"],d["regressed"]),row.names=F,quote=F,file=sprintf("tst-%s-iypr.csv",ntst))
 	n=nrow(d)
 	se=sum((d["regressed"] - d["pbindaff"])^2)
 	rmse=sqrt(se/n)
@@ -16,5 +16,5 @@ for (v in c(2007,2012,2013))
 	pcor=cor(d["regressed"], d["pbindaff"], method="pearson")
 	scor=cor(d["regressed"], d["pbindaff"], method="spearman")
 	kcor=cor(d["regressed"], d["pbindaff"], method="kendall")
-	cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor),file=sprintf("pdbbind-%s-tst-stat.csv",v))
+	cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor),file=sprintf("tst-%s-stat.csv",ntst))
 }
