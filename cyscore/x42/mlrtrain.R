94a71d91af733df72cec4ebfa528fe8e5dd94492
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-31 17:19:06 +0800

    Added x42

---
+++ cyscore/x42/mlrtrain.R
@@ -0,0 +1,7 @@
+#!/usr/bin/env Rscript
+args=commandArgs(trailingOnly=T)
+ntrn=args[1]
+trn=read.csv(sprintf("../trn-%s-yxi.csv",ntrn))
+c=coefficients(lm(pbindaff~gauss1+gauss2+repulsion+hydrophobic+hydrogenbonding+flexibility+X6.6+X7.6+X8.6+X16.6+X6.7+X7.7+X8.7+X16.7+X6.8+X7.8+X8.8+X16.8+X6.16+X7.16+X8.16+X16.16+X6.15+X7.15+X8.15+X16.15+X6.9+X7.9+X8.9+X16.9+X6.17+X7.17+X8.17+X16.17+X6.35+X7.35+X8.35+X16.35+X6.53+X7.53+X8.53+X16.53,trn))
+c[is.na(c)]=0
+write.csv(c,quote=F,file=sprintf("trn-%s-coef.csv",ntrn))
