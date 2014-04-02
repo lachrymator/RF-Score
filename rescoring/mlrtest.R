b7b56efdf293ae2e7eb7e0811f3b3aaf17717c95
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-04-02 11:05:35 +0800

    Renamed pdbbind-$v-trn-coef.csv to pdbbind-$v.csv

--- rescoring/mlrtest.R
+++ rescoring/mlrtest.R
@@ -3,7 +3,7 @@ args=commandArgs(trailingOnly=T)
 v=args[1]
 w=as.numeric(args[2])
 t=args[3]
-c=read.csv(sprintf("pdbbind-%s-trn-coef.csv",v))[,2]
+c=read.csv(sprintf("pdbbind-%s.csv",v))[,2]
 d=read.csv(ifelse(t=="tst","../tst-yxi.csv",sprintf("../pdbbind-%s-trn-yxi.csv",v))) # d[1] is pbindaff in pKd unit, d[2] to d[6] are Vina's 5 unweighted terms, d[7] and d[8] are N(ActTors) and N(InactTors).
 d["t"]=1+w*(d["nacttors"]+0.5*d["ninacttors"]) # Flexibility penalty = 1 + w * Nrot. In Vina's implementation, active torsions are counted as 1, while inactive torsions (i.e. -OH,-NH2,-CH3) are counted as 0.5.
 d["predicted"]=(c[1]+d[2]*c[2]+d[3]*c[3]+d[4]*c[4]+d[5]*c[5]+d[6]*c[6])/d["t"] # Binding affinity predicted by the newly-trained model, with kcal/mol converted to pKd.
