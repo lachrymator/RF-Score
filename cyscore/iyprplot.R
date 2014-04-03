fe2a316d4202c2a644481b50d929c72022baaf8a
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-04-03 16:46:53 +0800

    Removed d["regressed"]

--- cyscore/iyprplot.R
+++ cyscore/iyprplot.R
@@ -3,17 +3,13 @@ args=commandArgs(trailingOnly=T)
 ntrn=args[1]
 ntst=args[2]
 d=read.csv(sprintf("trn-%s-tst-%s-iyp.csv",ntrn,ntst))
-r=lm(pbindaff~predicted,d)
-d["regressed"]=fitted(r)
-write.csv(c(d["PDB"],d["pbindaff"],d["predicted"],round(d["regressed"],2)),row.names=F,quote=F,file=sprintf("trn-%s-tst-%s-iypr.csv",ntrn,ntst))
 n=nrow(d) # Number of samples.
 xylim=c(0,14)
-se=sum((d["regressed"] - d["pbindaff"])^2) # Square error.
-rmse=sqrt(se/n)
-sdev=sqrt(se/(n-2))
-pcor=cor(d["regressed"], d["pbindaff"], method="pearson")
-scor=cor(d["regressed"], d["pbindaff"], method="spearman")
-kcor=cor(d["regressed"], d["pbindaff"], method="kendall")
+rmse=sqrt(sum((d["predicted"] - d["pbindaff"])^2)/n)
+sdev=summary(lm(pbindaff~predicted,d))$sigma
+pcor=cor(d["predicted"], d["pbindaff"], method="pearson")
+scor=cor(d["predicted"], d["pbindaff"], method="spearman")
+kcor=cor(d["predicted"], d["pbindaff"], method="kendall")
 cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor), file=sprintf("trn-%s-tst-%s-stat.csv",ntrn,ntst))
 png(sprintf("trn-%s-tst-%s-yp.png",ntrn,ntst),bg="transparent",width=960,height=960,res=120)
 par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
