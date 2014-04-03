5e6b58bd62892bb6247417609cc45e6ff99235c3
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-04-03 19:00:15 +0800

    Added c[1] to d["regressed"]

--- cyscore/x4/mlrtest.R
+++ cyscore/x4/mlrtest.R
@@ -4,5 +4,5 @@ ntrn=args[1]
 ntst=args[2]
 c=read.csv(sprintf("trn-%s.csv",ntrn))[,2]
 d=read.csv(sprintf("../tst-%s-yxi.csv",ntst))
-d["predicted"]=c[2]*d[2]+c[3]*d[3]+c[4]*d[4]+c[5]*d[5]
+d["predicted"]=c[1]+c[2]*d[2]+c[3]*d[3]+c[4]*d[4]+c[5]*d[5]
 write.csv(c(d["PDB"],d["pbindaff"],round(d["predicted"],2)),row.names=F,quote=F,file=sprintf("trn-%s-tst-%s-iyp.csv",ntrn,ntst))
