d46969efd7090fcab0788c7959a759f4c452d748
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-04-03 11:22:43 +0800

    Updated iyprplot.R to compute sdev according to Wang's SD and to compute rmse between y and p

--- rescoring/iyprplot.R
+++ rescoring/iyprplot.R
@@ -6,18 +6,20 @@ c=read.csv(sprintf("pdbbind-%s-trn-coef.csv",v))[,2]
 d=read.csv(sprintf("pdbbind-%s-%s-iyp.csv",v,t))
 d["regressed"]=c[1]+c[2]*d["predicted"]
 write.csv(c(d["PDB"],d["pbindaff"],d["predicted"],round(d["regressed"],2)),row.names=F,quote=F,file=sprintf("pdbbind-%s-%s-iypr.csv",v,t))
-n=nrow(d) # Number of samples.
-se=sum(residuals(lm(d[,"pbindaff"]~d[,"predicted"]))^2) # Square error.
-rmse=sqrt(se/n)
-sdev=sqrt(se/(n-2))
+n=nrow(d)
+sdev=summary(lm(pbindaff~predicted,d))$sigma
 pcor=cor(d["predicted"], d["pbindaff"], method="pearson")
 scor=cor(d["predicted"], d["pbindaff"], method="spearman")
 kcor=cor(d["predicted"], d["pbindaff"], method="kendall")
-cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor), file=sprintf("pdbbind-%s-%s-stat.csv",v,t))
 xylim=c(0,14)
 if (n == 382) xylim=c(0,12)
 for (p in c("predicted","regressed"))
 {
+	rmse=sqrt(sum((d[p]-d["pbindaff"])^2)/n)
+	if (p == "predicted")
+	{
+		cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.3f,%.3f,%.3f,%.3f,%.3f\n", n, rmse, sdev, pcor, scor, kcor), file=sprintf("pdbbind-%s-%s-stat.csv",v,t))
+	}
 	l=substr(p,1,1)
 	tiff(sprintf("pdbbind-%s-%s-y%s.tiff",v,t,l),compress="lzw")
 	par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
