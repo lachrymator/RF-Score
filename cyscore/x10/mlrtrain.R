bb61b72c6c3ea14c786a84a64f8601957ac36f09
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-04-08 15:06:54 +0800

    Renamed a variable to avoid conflict

--- cyscore/x10/mlrtrain.R
+++ cyscore/x10/mlrtrain.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 args=commandArgs(trailingOnly=T)
 trn=args[1]
-trn=read.csv(sprintf("../trn-%s-yxi.csv",trn))
-c=coefficients(lm(pbindaff~Hydrophobic+Vdw+HBond+Ent+gauss1+gauss2+repulsion+hydrophobic+hydrogenbonding+flexibility,trn))
+d=read.csv(sprintf("../trn-%s-yxi.csv",trn))
+c=coefficients(lm(pbindaff~Hydrophobic+Vdw+HBond+Ent+gauss1+gauss2+repulsion+hydrophobic+hydrogenbonding+flexibility,d))
 c[is.na(c)]=0
 write.csv(c,quote=F,file=sprintf("trn-%s.csv",trn))
