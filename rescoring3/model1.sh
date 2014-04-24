20dc4c6326b13a2809b4f1cbcd3d20eed4a89bbf
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-04-24 10:15:09 +0800

    Renamed f to tst

--- rescoring3/model1.sh
+++ rescoring3/model1.sh
@@ -4,9 +4,9 @@ for m in 1; do
 	for s in 3; do
 		echo set$s
 		cd set$s
-		for f in {0..5}; do
-			echo family$f
-			cut -d, -f2 ../../set3/tst-$f-iy.csv | paste -d, - pdbbind-2007-tst-$f-p.csv | rf-stat > pdbbind-2007-tst-$f-stat.csv
+		for tst in {0..5}; do
+			echo tst$tst
+			cut -d, -f2 ../../set3/tst-$tst-iy.csv | paste -d, - pdbbind-2007-tst-$tst-p.csv | rf-stat > pdbbind-2007-tst-$tst-stat.csv
 		done
 		cd ..
 	done
