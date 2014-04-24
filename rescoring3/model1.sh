f182acc244327d332ce3ff892687eb9300a74fb4
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-04-24 15:01:49 +0800

    Added -trn-0 to model1 files

--- rescoring3/model1.sh
+++ rescoring3/model1.sh
@@ -6,8 +6,8 @@ for m in 1; do
 		cd set$s
 		for tst in {0..5}; do
 			echo tst$tst
-			cut -d, -f2 ../../set3/tst-$tst-iy.csv | paste -d, - pdbbind-2007-tst-$tst-p.csv | rf-stat > pdbbind-2007-tst-$tst-stat.csv
-			rm pdbbind-2007-tst-$tst-p.csv
+			cut -d, -f2 ../../set3/tst-$tst-iy.csv | paste -d, - pdbbind-2007-trn-0-tst-$tst-p.csv | rf-stat > pdbbind-2007-trn-0-tst-$tst-stat.csv
+			rm pdbbind-2007-trn-0-tst-$tst-p.csv
 		done
 		cd ..
 	done
