1c249151b1d3b9e5d21e67d6514c6db3b4f2aa6d
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-05-02 17:53:01 +0800

    Updated statplot.R

--- cyscore/statplot.R
+++ cyscore/statplot.R
@@ -1,42 +1,45 @@
 #!/usr/bin/env Rscript
-nx=6 # Number of features.
-ntrns=7 # Number of training sets.
-nc=4 # Number of performance measures.
-xs=c(2,4,10,40,42,46)
-trns=c(247,1105,2280,792,1300,2059,2897)
+ms=c("mlr","rf","rf","rf")
+xs=c(4,4,10,46)
+trns=c(592,1184,1776,2367)
+tst=592
 statc=c("rmse","sdev","pcor","scor")
 statx=c("RMSE","SD","Rp","Rs")
-for (m in c("mlr","rf"))
+nm=length(ms) # Number of models.
+ntrn=length(trns) # Number of training sets.
+nc=length(statc) # Number of performance measures.
+ts=array(nm)
+for (mi in 1:nm)
 {
-	for (tst in c(195,201,382))
+	ts[mi]=paste(toupper(ms[mi]),xs[mi],sep="-x")
+}
+#setEPS()
+med=array(dim=c(nm,ntrn,nc))
+for (trni in 1:ntrn)
+{
+	trn=trns[trni]
+	for (mi in 1:nm)
 	{
-		box=array(list(),dim=c(nx,ntrns,nc))
-		for (trni in 1:ntrns)
-		{
-			trn=trns[trni]
-			for (xi in 1:nx)
-			{
-				stat=read.csv(sprintf("x%d/%s/trn-%d-tst-%d-stat.csv",xs[xi],m,trn,tst))
-				for (ci in 1:nc)
-				{
-					box[xi,trni,ci]=stat[statc[ci]]
-				}
-			}
-		}
+		stat=read.csv(sprintf("x%d/%s/trn-%d-tst-%d-stat.csv",xs[mi],ms[mi],trn,tst))
 		for (ci in 1:nc)
 		{
-			png(sprintf("%s/tst-%d-%s.png",m,tst,statc[ci]),bg="transparent",width=960,height=960,res=120)
-			par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
-			for (xi in 1:nx)
-			{
-				boxplot(box[xi,,ci],xaxt="n",yaxt="n",xlab="",ylab="",border=xi)
-				par(new=T)
-			}
-			title(xlab="Number of training complexes",ylab=statx[ci])
-			legend(ifelse(ci<=2,"topright","bottomright"),title="Features",legend=xs,fill=1:nx,cex=1.3)
-			axis(1,at=1:ntrns,labels=trns)
-			axis(2)
-			dev.off()
+			med[mi,trni,ci]=median(stat[statc[ci]][,])
 		}
 	}
+}
+for (ci in 1:nc)
+{
+	ylim=c(min(med[,,ci],na.rm=T),max(med[,,ci],na.rm=T))
+	png(sprintf("tst-%d-%s.png",tst,statc[ci]),bg="transparent",width=960,height=960,res=120)
+	par(cex.lab=1.3,cex.axis=1.3,cex.main=1.3)
+	for (mi in 1:nm)
+	{
+		plot(trns,med[mi,,ci],ylim=ylim,type="b",xaxt="n",yaxt="n",xlab="",ylab="",pch=mi,col=mi)
+		par(new=T)
+	}
+	title(xlab="Number of training complexes",ylab=statx[ci])
+	legend("right",title="Models",legend=ts,fill=1:nm,cex=1.3)
+	axis(1)
+	axis(2)
+	dev.off()
 }
