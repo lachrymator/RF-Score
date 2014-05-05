60e8506189035b4acccd89f5964169edba2db83a
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-05-05 10:26:12 +0800

    Renamed m$trn to 0$trn

--- rescoring3/model4prepare.sh
+++ rescoring3/model4prepare.sh
@@ -39,9 +39,9 @@ for m in 2 3 4; do
 				grep ",$c" pdbbind-2010-trn-0-yxi.csv >> pdbbind-2010-trn-$trn-yxi.csv
 			done
 			echo pdbbind-2010-trn-m$trn-yxi.csv
-			head -1 pdbbind-2010-trn-0-yxi.csv > pdbbind-2010-trn-m$trn-yxi.csv
+			head -1 pdbbind-2010-trn-0-yxi.csv > pdbbind-2010-trn-0$trn-yxi.csv
 			for c in $(cut -d, -f1 ../../set3/pdbbind-2010-trn-0-iy.csv ../../set3/pdbbind-2010-trn-$trn-iy.csv | sort | uniq -u); do
-				grep ",$c" pdbbind-2010-trn-0-yxi.csv >> pdbbind-2010-trn-m$trn-yxi.csv
+				grep ",$c" pdbbind-2010-trn-0-yxi.csv >> pdbbind-2010-trn-0$trn-yxi.csv
 			done
 		done
 		cd ..
