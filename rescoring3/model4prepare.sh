07edf9aa194af4f792e79326974b4c57739d734f
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-05-05 11:05:56 +0800

    Updated model4prepare.sh to use for trn in 1 2 3 4 5 01 02 03 04 05; do

--- rescoring3/model4prepare.sh
+++ rescoring3/model4prepare.sh
@@ -32,13 +32,11 @@ for m in 2 3 4; do
 	for s in 3; do
 		cd set$s
 		echo set$s
-		for trn in {1..5}; do
-			for m in "" "0"; do
-				echo pdbbind-2010-trn-$m$trn-yxi.csv
-				head -1 pdbbind-2010-trn-0-yxi.csv > pdbbind-2010-trn-$m$trn-yxi.csv
-				for c in $([[ $m == "" ]] && cut -d, -f1 ../../set3/pdbbind-2010-trn-$trn-iy.csv || cut -d, -f1 ../../set3/pdbbind-2010-trn-0-iy.csv ../../set3/pdbbind-2010-trn-$trn-iy.csv | sort | uniq -u); do
-					grep ",$c" pdbbind-2010-trn-0-yxi.csv >> pdbbind-2010-trn-$m$trn-yxi.csv
-				done
+		for trn in 1 2 3 4 5 01 02 03 04 05; do
+			echo pdbbind-2010-trn-$trn-yxi.csv
+			head -1 pdbbind-2010-trn-0-yxi.csv > pdbbind-2010-trn-$trn-yxi.csv
+			for c in $([[ ${#m} == 1 ]] && cut -d, -f1 ../../set3/pdbbind-2010-trn-$trn-iy.csv || cut -d, -f1 ../../set3/pdbbind-2010-trn-0-iy.csv ../../set3/pdbbind-2010-trn-${trn:1:1}-iy.csv | sort | uniq -u); do
+				grep ",$c" pdbbind-2010-trn-0-yxi.csv >> pdbbind-2010-trn-$trn-yxi.csv
 			done
 		done
 		cd ..
