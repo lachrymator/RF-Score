16eb056bd016f57d32e5726c5f3a42d113a3f5ef
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-05-07 22:12:37 +0800

    Used cut to replace cat

--- rescoring3/model1prepare.sh
+++ rescoring3/model1prepare.sh
@@ -7,8 +7,7 @@ for m in 1; do
 		echo set$s
 		for tst in {0..5}; do
 			echo tst$tst
-			for iy in $(cat $pdbbind/v2013/rescoring-3-set-$s-tst-$tst-iy.csv); do
-				c=${iy:0:4}
+			for c in $(cut -d, -f1 $pdbbind/v2013/rescoring-3-set-$s-tst-$tst-iy.csv); do
 				tail -n +18 $pdbbind/v2013/${c}/out/${c}_ligand_ligand_1.txt | head -1
 			done | cut -d' ' -f2 | awk '{printf "%.2f\n", $1*-0.73349480509}' > pdbbind-2007-trn-0-tst-$tst-p.csv
 		done
