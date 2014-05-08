8cb1b868d675aff9e71a967f536b43210e4a4557
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-05-08 15:38:33 +0800

    Updated rf-stat.R to use the same input format of rf-stat

--- bin/rf-stat.R
+++ bin/rf-stat.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
-d=read.csv(file("stdin"))
+d=read.table(file("stdin"),sep=",",header=F)
 n=nrow(d)
-rmse=sqrt(sum((d["pbindaff"]-d["predicted"])^2)/n)
-sdev=summary(lm(pbindaff~predicted,d))$sigma
-pcor=cor(d["pbindaff"],d["predicted"],method="pearson")
-scor=cor(d["pbindaff"],d["predicted"],method="spearman")
-kcor=cor(d["pbindaff"],d["predicted"],method="kendall")
+rmse=sqrt(sum((d[1]-d[2])^2)/n)
+sdev=summary(lm(V1~V2,d))$sigma
+pcor=cor(d[1],d[2],method="pearson")
+scor=cor(d[1],d[2],method="spearman")
+kcor=cor(d[1],d[2],method="kendall")
 cat(sprintf("n,rmse,sdev,pcor,scor,kcor\n%d,%.2f,%.2f,%.3f,%.3f,%.3f\n",n,rmse,sdev,pcor,scor,kcor))
